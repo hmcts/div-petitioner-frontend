8edb163fc7e8e71be6f717699acaa33745d4b797
niyioyelade <Bukola01>
2018-03-12 11:55:09 +0000

    DIV-2000-Logout End to end test added

---
+++ test/end-to-end/paths/logout.js
@@ -0,0 +1,24 @@
+const toggleStore = require('test/end-to-end/helpers/featureToggleStore.js');
+const idamConfigHelper = require('test/end-to-end/helpers/idamConfigHelper.js');
+
+Feature('Logout Session');
+
+Scenario('Logount on Save and close', function (I) {
+  I.amOnPage('/index');
+
+  I.startApplication();
+
+  I.haveBrokenMarriage();
+  I.haveRespondentAddress();
+  I.haveMarriageCert();
+
+  I.clickSaveAndCLose();
+  I.seeCurrentUrlEquals('/exit/application-saved');
+
+  if (toggleStore.getToggle('idam')) {
+    I.see(idamConfigHelper.getTestEmail());
+  }
+
+  I.click('Back');
+  I.startApplication();
+});

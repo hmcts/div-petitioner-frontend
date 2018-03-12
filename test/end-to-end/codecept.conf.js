e7bc534becfc2d63e885dd007dfb6ca7dd838fba
Rob Doherty <robertdoherty@hotmail.co.uk>
2018-03-12 16:54:08 +0000

    Added method to parallelise e2e tests by Feature

--- test/end-to-end/codecept.conf.js
+++ test/end-to-end/codecept.conf.js
@@ -1,12 +1,18 @@
+const glob = require('glob');
+const fsPath = require('path');
+const fs = require('fs');
 const waitForTimeout = parseInt(process.env.E2E_WAIT_FOR_TIMEOUT_VALUE) || 10000;
 const waitForAction = parseInt(process.env.E2E_WAIT_FOR_ACTION_VALUE) || 1000;
+const testsToRun = './paths/**/*.js';
+const outputDir = './output';
 
 console.log('waitForAction value set to', waitForAction); // eslint-disable-line no-console
 
 exports.config = {
-  'tests': './paths/**/*.js',
-  'output': './output',
+  'tests': testsToRun,
+  'output': outputDir,
   'timeout': 1000,
+  'multiple': getFeatureNames(),
   'helpers': {
     'Nightmare': {
       'url': process.env.E2E_FRONTEND_URL || 'https://localhost:8080',
@@ -55,3 +61,31 @@ exports.config = {
   },
   'name': 'frontEnd Tests'
 };
+
+function getFeatureNames() {
+
+  let testFiles = {};
+
+  glob.sync(fsPath.resolve(__dirname, testsToRun)).forEach((file) => {
+
+    // Collect features to grep for and test via 'codeceptjs run-multiple'
+    let featureName;
+    fs.readFileSync(file).toString().split('\n').forEach(function (line) {
+      if (line.indexOf('Feature(') > -1) {
+        featureName = (line.split('\'')[1]).replace('\\', '');
+      }
+    });
+
+    // Build test runners that only run single features
+    testFiles[featureName] = {
+      'grep': '(?=.*' + featureName + ')',
+      'browsers': ['chrome']
+    };
+  });
+
+  return testFiles;
+}
+
+function escapeRegExp(text) { // eslint-disable-line no-unused-vars
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}

4abb64f892f755a7137e3d627fcd7ecb68fac273
Dominic Taylor <dom.taylor7@gmail.com>
2018-03-15 13:11:09 +0000

    fix auth page

--- app/steps/authenticated/index.js
+++ app/steps/authenticated/index.js
@@ -7,7 +7,9 @@ const sessionTimeout = require('app/middleware/sessionTimeout');
 const idamLandingPage = (req, res, next) => {
   if (features.idam) {
     const landing = idam.landingPage();
-    return landing(req, res, next);
+    return landing(req, res, () => {
+      next();
+    });
   }
 
   return next();

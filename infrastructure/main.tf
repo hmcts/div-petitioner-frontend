964f39f483a5b7984de556a2e0b70049684e8678
Tony Chow <tony.chow@hmcts.net>
2018-03-21 15:46:43 +0000

    Fixed incorrect IDAM health endpoint

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -75,7 +75,7 @@ module "frontend" {
 
     // IDAM
     IDAM_API_URL = "${var.idam_api_url}"
-	  IDAM_APP_HEALHCHECK_URL ="${var.idam_api_url}${var.status_health_endpoint}"
+	  IDAM_APP_HEALHCHECK_URL ="${var.idam_api_url}${var.health_endpoint}"
     IDAM_LOGIN_URL = "${var.idam_authentication_web_url}${var.idam_authentication_login_endpoint}"
     IDAM_AUTHENTICATION_HEALHCHECK_URL = "${var.idam_authentication_web_url}${var.health_endpoint}"
     IDAM_SECRET = "${data.vault_generic_secret.idam_secret.data["value"]}"

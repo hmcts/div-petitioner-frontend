f6aae15d1e5f9dc085f3e01e1f3e0468595e375c
Rob Doherty <robertdoherty@hotmail.co.uk>
2018-03-22 18:23:44 +0000

    DIV-1976: added fees tests to e2e to ensure integrations work

--- test/end-to-end/pages/pay/by-card-online-on-govpay.js
+++ test/end-to-end/pages/pay/by-card-online-on-govpay.js
@@ -55,6 +55,7 @@ function cancelOnGovPay() {
 function onGovPay(I) {
   I.waitForText('Enter card details', 20);
   I.seeInCurrentUrl('www.payments.service.gov.uk/card_details');
+  I.see('£550.00');
 }
 
 module.exports = { payOnGovPay, payOnGovPayFailure, cancelOnGovPay };

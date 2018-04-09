1b6427823d80274fee7dfae3a2e2d78432814141
Tony Chow <tony.chow@hmcts.net>
2018-04-09 14:42:51 +0100

    Added Case Progression healthcheck back in

--- app/services/healthcheck.js
+++ app/services/healthcheck.js
@@ -55,12 +55,12 @@ router.get('/health', healthcheck.configure({
         return !error && res.status === OK ? outputs.up() : outputs.down(error);
       }
     }),
-    // 'transformation-api': healthcheck.web(config.services.transformation.health, {
-    //   callback: (error, res) => { // eslint-disable-line id-blacklist
-    //     logger.error(`Health check failed on transformation-api: ${error}`);
-    //     return !error && res.status === OK ? outputs.up() : outputs.down(error);
-    //   }
-    // }),
+    'case-progression': healthcheck.web(config.services.transformation.health, {
+      callback: (error, res) => { // eslint-disable-line id-blacklist
+        logger.error(`Health check failed on transformation-api: ${error}`);
+        return !error && res.status === OK ? outputs.up() : outputs.down(error);
+      }
+    }),
     'service-auth-provider-api': healthcheck.web(config.services.serviceAuthProvider.health, {
       callback: (error, res) => { // eslint-disable-line id-blacklist
         logger.error(`Health check failed on service-auth-provider-api: ${error}`);

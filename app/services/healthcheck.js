d9c76303bc044d86226a46e7c1ece9862b7d95a3
Tony Chow <tony.chow@hmcts.net>
2018-04-10 12:04:29 +0100

    Fixed sandbox pipeline for all envs and using call to http healthcheck instead as https will return an error

--- app/services/healthcheck.js
+++ app/services/healthcheck.js
@@ -19,6 +19,10 @@ client.on('error', error => {
   logger.error(error);
 });
 
+const changeHttpsToHttp = url => {
+  url.replace('https://', 'http://');
+};
+
 router.get('/health', healthcheck.configure({
   checks: {
     redis: healthcheck.raw(() => {
@@ -49,13 +53,13 @@ router.get('/health', healthcheck.configure({
         return !error && res.status === OK ? outputs.up() : outputs.down(error);
       }
     }),
-    'evidence-management-client-api': healthcheck.web(config.evidenceManagmentClient.health, {
+    'evidence-management-client-api': healthcheck.web(changeHttpsToHttp(config.evidenceManagmentClient.health), {
       callback: (error, res) => { // eslint-disable-line id-blacklist
         logger.error(`Health check failed on evidence-management-client-api: ${error}`);
         return !error && res.status === OK ? outputs.up() : outputs.down(error);
       }
     }),
-    'case-progression': healthcheck.web(config.services.transformation.health, {
+    'case-progression': healthcheck.web(changeHttpsToHttp(config.services.transformation.health), {
       callback: (error, res) => { // eslint-disable-line id-blacklist
         logger.error(`Health check failed on case-progression: ${error}`);
         return !error && res.status === OK ? outputs.up() : outputs.down(error);

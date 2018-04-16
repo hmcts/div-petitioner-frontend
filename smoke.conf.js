5003bb7e45529b7d274e692263de8085f960c200
Tony Chow <tony.chow@hmcts.net>
2018-04-16 14:29:33 +0100

    Add headless header to nightmare test

---
+++ smoke.conf.js
@@ -0,0 +1,15 @@
+exports.config = {
+  tests: './test/end-to-end/smoke/*.js',
+  timeout: 1000,
+  helpers: {
+    Nightmare: {
+      url: process.env.TEST_URL || process.env.E2E_FRONTEND_URL || 'https://localhost:8080',
+      show: false,
+      switches: {
+        'ignore-certificate-errors': true,
+        'proxy-server': process.env.E2E_PROXY_SERVER || ''
+      }
+    }
+  },
+  name: 'frontend Smoke Tests'
+};

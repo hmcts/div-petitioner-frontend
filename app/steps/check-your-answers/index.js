4a5e85a1b4c72c327f7cb33eb876dbf6da3dec82
richard-diamond <richard.diamond@hmcts.net>
2018-04-18 08:50:20 +0100

    DIV-1932: implemented PR review changes

--- app/steps/check-your-answers/index.js
+++ app/steps/check-your-answers/index.js
@@ -166,7 +166,7 @@ module.exports = class CheckYourAnswers extends ValidationStep {
     let templates = [];
 
     // Do not render the same template more than once
-    // if the application is attempting to render question more than once
+    // if the application is attempting to render question more than once 
     // we know user has not completed the questions and can show answered
     // questions up to this point
     if (previousQuestionsRendered.includes(step.url)) {

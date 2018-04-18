efc234e4cb21ef4fd11da3a3ce5ba61ea2aa2751
richard-diamond <richard.diamond@hmcts.net>
2018-04-18 08:26:13 +0100

    DIV-1932: implemented PR review changes

--- mutation-test.sh
+++ mutation-test.sh
@@ -4,7 +4,7 @@ echo "start: $(date)"
 
 for file in ./test/mutation-testing/stryker_*.conf.js ; do
   echo "file: $file";
-  yarn test-stryker $file ;
+  yarn test:stryker $file ;
 done
 
 echo "end: $(date)"

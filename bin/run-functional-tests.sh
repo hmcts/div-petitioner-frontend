8a8b5e3bc772085ac97bbaf29eb94816077ebc0e
Tony Chow <tony.chow@hmcts.net>
2018-04-20 12:18:49 +0100

    Output steps on functional and smoke tests. Shutdown functional tests properly after exit

--- bin/run-functional-tests.sh
+++ bin/run-functional-tests.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -ex
 
+COMPOSE_FILE="docker/test-compose.yaml"
+
+# Trap errors or exits and close everything
+function shutdownDocker() {
+ docker-compose -f ${COMPOSE_FILE} down
+}
+
+trap shutdownDocker INT TERM QUIT EXIT
+
 # Setup required environment variables. TEST_URL should be set by CNP
 export E2E_FRONTEND_URL=${TEST_URL}
 export E2E_FRONTEND_NODE_ENV="production"
@@ -11,4 +20,5 @@ export COURT_PHONENUMBER="0300 303 0642"
 export COURT_OPENINGHOURS="Monday to Friday, 8.30am to 5pm"
 export COURT_EMAIL="Divorce_Reform_Pro@Justice.gov.uk"
 
-docker-compose -f docker/test-compose.yaml run functional-tests
+docker-compose -f ${COMPOSE_FILE} run functional-tests
+shutdownDocker

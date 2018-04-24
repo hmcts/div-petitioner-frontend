418a13a2d59448e1dc8d160fdbf22c1272d752dd
Mathan Thava <mathan.thavachelvam@hmcts.net>
2018-04-24 16:33:34 +0100

    remove key vault module

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -163,14 +163,3 @@ module "frontend" {
     NORTHWEST_COURTWEIGHT = "${var.court_northwest_court_weight}"
   }
 }
-
-module "key-vault" {
-  source              = "git@github.com:hmcts/moj-module-key-vault?ref=master"
-  name                = "${local.vaultName}"
-  product             = "${var.product}"
-  env                 = "${var.env}"
-  tenant_id           = "${var.tenant_id}"
-  object_id           = "${var.jenkins_AAD_objectId}"
-  resource_group_name = "${module.frontend.resource_group_name}"
-  product_group_object_id = "68839600-92da-4862-bb24-1259814d1384"
-}

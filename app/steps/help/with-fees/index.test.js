fe6e9a894afbcb5e106ca21e6b5a828ade3774fe
David Gimelle <davidgimelle@gmail.com>
2018-04-25 15:10:13 +0100

    add unit for No and uncorrect fee ref

--- app/steps/help/with-fees/index.test.js
+++ app/steps/help/with-fees/index.test.js
@@ -67,6 +67,16 @@ describe(modulePath, () => {
         s.steps.ExitNoHelpWithFees);
     });
 
+    it('redirects to the next page when No selected and HWF number uncorrect', done => {
+      const context = {
+        helpWithFeesAppliedForFees: 'No',
+        helpWithFeesReferenceNumber: 'HWF'
+      };
+
+      testRedirect(done, agent, underTest, context,
+        s.steps.ExitNoHelpWithFees);
+    });
+
     it('redirects to the next page when Yes selected and HWF number correct', done => {
       const context = {
         helpWithFeesAppliedForFees: 'Yes',

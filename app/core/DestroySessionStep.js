77e5e931b40545967610a4807199432b4d251402
Warren Taggart <warren.taggart@hmcts.net>
2018-04-27 12:14:59 +0100

    DIV-2336 Reverting unwanted changes

--- app/core/DestroySessionStep.js
+++ app/core/DestroySessionStep.js
@@ -1,6 +1,5 @@
 const Step = require('./Step');
 const idam = require('app/services/idam');
-const initSession = require('app/middleware/initSession');
 
 
 module.exports = class DestroySessionStep extends Step {
@@ -15,7 +14,7 @@ module.exports = class DestroySessionStep extends Step {
   }
 
   get middleware() {
-    return [idam.logout(), initSession];
+    return [idam.logout()];
   }
 
   get nextStep() {

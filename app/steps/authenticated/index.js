8c889e7ff972484a48b351e287f5e98ff6e36f3a
Dominic Taylor <dom.taylor7@gmail.com>
2018-05-02 12:19:56 +0100

    if user logs into idam with expired session, destroy it

--- app/steps/authenticated/index.js
+++ app/steps/authenticated/index.js
@@ -2,12 +2,23 @@ const Step = require('app/core/steps/Step');
 const idam = require('app/services/idam');
 const { features } = require('@hmcts/div-feature-toggle-client')().featureToggles;
 const initSession = require('app/middleware/initSession');
-const sessionTimeout = require('app/middleware/sessionTimeout');
 
 const idamLandingPage = (req, res, next) => {
   if (features.idam) {
     const landing = idam.landingPage();
-    return landing(req, res, next);
+    return landing(req, res, () => {
+      const { session } = req;
+
+      // if the previous session has expired
+      // after user logged in then destroy it
+      if (session.expires <= Date.now()) {
+        req.session.destroy(() => {
+          next();
+        });
+      } else {
+        next();
+      }
+    });
   }
 
   return next();
@@ -25,7 +36,6 @@ module.exports = class Authenticated extends Step {
   get middleware() {
     return [
       initSession,
-      sessionTimeout,
       idamLandingPage
     ];
   }

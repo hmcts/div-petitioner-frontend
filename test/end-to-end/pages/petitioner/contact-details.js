0cf8891d3991762f345e0ef5dfeff891e7f60447
niyioyelade <oyeniyi.oyelade>
2018-05-03 09:20:41 +0100

    DIV-2229 'generic-error' is thrown when trying to save and close invalid email addresses on 'contact-details' - fixed end to end test.

--- test/end-to-end/pages/petitioner/contact-details.js
+++ test/end-to-end/pages/petitioner/contact-details.js
@@ -1,9 +1,11 @@
 const content = require('app/steps/petitioner/contact-details/content.json').resources.en.translation.content;
+const idamConfigHelper = require('test/end-to-end/helpers/idamConfigHelper.js');
 
 function enterPetitionerContactDetails() {
   const I = this;
 
   I.seeCurrentUrlEquals('/petitioner-respondent/contact-details');
+  I.see(idamConfigHelper.getTestEmail());
   I.fillField('petitionerPhoneNumber', '01234567890');
   I.checkOption(content.petitionerConsent);
   I.click('Continue');

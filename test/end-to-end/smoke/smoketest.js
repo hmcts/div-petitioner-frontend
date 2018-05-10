3b2a8e1955aa586571685300ecacc98e4029eed9
Tony Chow <36265570+tchow8@users.noreply.github.com>
2018-05-10 09:02:42 +0100

    Use amOnPage instead of the loaded for default index

--- test/end-to-end/smoke/smoketest.js
+++ test/end-to-end/smoke/smoketest.js
@@ -1,8 +1,8 @@
 const common = require('app/content/common.json').resources.en.translation;
 
-Feature('Smoke test', { retries: 1 });
+Feature('Smoke test', { retries: 2 });
 
 Scenario('Can see frontend index page', (I) => {
-  I.amOnLoadedPage('/index');
+  I.amOnPage('/index');
   I.see(common.continue);
 });

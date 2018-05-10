68abba156609a0085f9fd39cc07882e211af552b
Rob Doherty <robertdoherty@hotmail.co.uk>
2018-05-10 15:06:11 +0100

    re-introduced payment and upload tests, plus JSWait compatability

--- test/end-to-end/paths/uploadMarriageCertificate.js
+++ test/end-to-end/paths/uploadMarriageCertificate.js
@@ -1,58 +1,58 @@
-// const content = require('app/steps/marriage/upload/content.json').resources.en.translation.content;
+const content = require('app/steps/marriage/upload/content.json').resources.en.translation.content;
 
-// Feature('Upload Marriage Certificate');
+Feature('Upload Marriage Certificate');
 
-// Scenario('Test upload', function* (I) {
-//   I.amOnLoadedPage('/index');
-//   I.startApplication();
-//   I.haveBrokenMarriage();
-//   I.amOnLoadedPage('/petitioner-respondent/marriage-certificate-upload');
-//   const isDragAndDropSupported = yield I.checkElementExist('.dz-hidden-input');
-//   I.uploadMarriageCertificateFile(isDragAndDropSupported);
-// });
+Scenario('Test upload', function* (I) {
+  I.amOnLoadedPage('/index');
+  I.startApplication();
+  I.haveBrokenMarriage();
+  I.amOnLoadedPage('/petitioner-respondent/marriage-certificate-upload');
+  const isDragAndDropSupported = yield I.checkElementExist('.dz-hidden-input');
+  I.uploadMarriageCertificateFile(isDragAndDropSupported);
+});
 
-// Scenario('Test remove marriage Certificate', function* (I) {
-//   I.amOnLoadedPage('/index');
-//   I.startApplication();
-//   I.haveBrokenMarriage();
-//   I.amOnLoadedPage('/petitioner-respondent/marriage-certificate-upload');
-//   const isDragAndDropSupported = yield I.checkElementExist('.dz-hidden-input');
-//   I.deleteAMarriageCertificateFile(isDragAndDropSupported);
-// });
+Scenario('Test remove marriage Certificate', function* (I) {
+  I.amOnLoadedPage('/index');
+  I.startApplication();
+  I.haveBrokenMarriage();
+  I.amOnLoadedPage('/petitioner-respondent/marriage-certificate-upload');
+  const isDragAndDropSupported = yield I.checkElementExist('.dz-hidden-input');
+  I.deleteAMarriageCertificateFile(isDragAndDropSupported);
+});
 
-// Scenario('Test ability validate document type', function* (I) {
-//   I.amOnLoadedPage('/index');
-//   I.startApplication();
-//   I.haveBrokenMarriage();
-//   I.amOnLoadedPage('/petitioner-respondent/marriage-certificate-upload');
+Scenario('Test ability validate document type', function* (I) {
+  I.amOnLoadedPage('/index');
+  I.startApplication();
+  I.haveBrokenMarriage();
+  I.amOnLoadedPage('/petitioner-respondent/marriage-certificate-upload');
 
-//   const isDragAndDropSupported = yield I.checkElementExist('.dz-hidden-input');
+  const isDragAndDropSupported = yield I.checkElementExist('.dz-hidden-input');
 
-//   if(isDragAndDropSupported){
-//     // Test can upload .pdf
-//     I.testUploadResponse(isDragAndDropSupported, '/assets/test_pdf.pdf');
-//     I.dontSee(content.errorUnknown);
-//     I.dontSee(content.errorFileTypeInvalid);
+  if(isDragAndDropSupported){
+    // Test can upload .pdf
+    I.testUploadResponse(isDragAndDropSupported, '/assets/test_pdf.pdf');
+    I.dontSee(content.errorUnknown);
+    I.dontSee(content.errorFileTypeInvalid);
 
-//     // Test can upload .png
-//     I.testUploadResponse(isDragAndDropSupported, '/assets/test_png.png');
-//     I.dontSee(content.errorUnknown);
-//     I.dontSee(content.errorFileTypeInvalid);
+    // Test can upload .png
+    I.testUploadResponse(isDragAndDropSupported, '/assets/test_png.png');
+    I.dontSee(content.errorUnknown);
+    I.dontSee(content.errorFileTypeInvalid);
 
-//     // Test can upload .bmp
-//     I.testUploadResponse(isDragAndDropSupported, '/assets/test_bmp.bmp');
-//     I.dontSee(content.errorUnknown);
-//     I.dontSee(content.errorFileTypeInvalid);
+    // Test can upload .bmp
+    I.testUploadResponse(isDragAndDropSupported, '/assets/test_bmp.bmp');
+    I.dontSee(content.errorUnknown);
+    I.dontSee(content.errorFileTypeInvalid);
 
-//     // Test can upload .tiff
-//     I.testUploadResponse(isDragAndDropSupported, '/assets/test_tif.tif');
-//     I.dontSee(content.errorUnknown);
-//     I.dontSee(content.errorFileTypeInvalid);
+    // Test can upload .tiff
+    I.testUploadResponse(isDragAndDropSupported, '/assets/test_tif.tif');
+    I.dontSee(content.errorUnknown);
+    I.dontSee(content.errorFileTypeInvalid);
 
-//     // Test can NOT upload .zip
-//     I.testUploadResponse(isDragAndDropSupported, '/assets/image.zip');
-//     I.see(content.errorFileTypeInvalid);
-//   } else {
-//     I.say('JS upload disabled - skipping');
-//   }
-// });
+    // Test can NOT upload .zip
+    I.testUploadResponse(isDragAndDropSupported, '/assets/image.zip');
+    I.see(content.errorFileTypeInvalid);
+  } else {
+    I.say('JS upload disabled - skipping');
+  }
+});

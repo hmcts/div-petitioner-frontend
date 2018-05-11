35c0a7e808367b987b266f642f0735336a3916eb
richard-diamond <richard.diamond@hmcts.net>
2018-05-11 13:22:23 +0100

    DIV-1932: re-applied the changes made to incorporate stryker testing into the frontend

---
+++ test/mutation/stryker_3.conf.js
@@ -0,0 +1,47 @@
+const strykerConfiguration = config => {
+  config.set({
+    testRunner: 'mocha',
+    mutator: 'javascript',
+    transpilers: [],
+    reporter:
+      [
+        'clear-text',
+        'progress',
+        'html'
+      ],
+    testFramework: 'mocha',
+    coverageAnalysis: 'perTest',
+    mutate:
+      [
+        'app/services/**/*.js',
+        'app/steps/**/*.js',
+        'app/views/**/*.js',
+        '!app/services/**/*.test.js',
+        '!app/steps/**/*.test.js',
+        '!app/views/**/*.test.js'
+      ],
+    files: [ '**/*' ],
+    maxConcurrentTestRunners: 2,
+    symlinkNodeModules: false,
+    htmlReporter: { baseDir: 'functional-output/mutation-3' },
+    mochaOptions: {
+      files:
+        [
+          'app/services/**/*.test.js',
+          'app/steps/**/*.test.js',
+          'app/views/**/*.test.js'
+        ],
+      timeout: 8000
+    },
+    logLevel: 'debug',
+    plugins:
+      [
+        'stryker-mocha-runner',
+        'stryker-mocha-framework',
+        'stryker-javascript-mutator',
+        'stryker-html-reporter'
+      ]
+  });
+};
+
+module.exports = strykerConfiguration;

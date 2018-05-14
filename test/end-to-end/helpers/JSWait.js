28a1a3d5b17b92c7501c11c79449fa0bb6208db2
Tony Chow <tony.chow@hmcts.net>
2018-05-14 20:20:40 +0100

    Add more event listeners

--- test/end-to-end/helpers/JSWait.js
+++ test/end-to-end/helpers/JSWait.js
@@ -4,6 +4,10 @@ class JSWait extends codecept_helper {
     const helper = this.helpers['WebDriverIO'] || this.helpers['Puppeteer'];
 
     helper.page.on('error', error => console.log(error)); // eslint-disable-line no-console
+    helper.page.on('pageerror', error => console.log(error)); // eslint-disable-line no-console
+    helper.page.on('requestfailed', error => console.log(error)); // eslint-disable-line no-console
+    helper.page.on('requestfinished', error => console.log(error)); // eslint-disable-line no-console
+    helper.page.on('response', error => console.log(error)); // eslint-disable-line no-console
   }
 
   _beforeStep(step) {

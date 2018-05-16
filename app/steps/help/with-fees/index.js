eb56a67946a9a440cbce61be294f2129bdab3127
David Gimelle <davidgimelle@gmail.com>
2018-05-16 14:34:37 +0100

    remove fee ref number of session

--- app/steps/help/with-fees/index.js
+++ app/steps/help/with-fees/index.js
@@ -34,6 +34,10 @@ module.exports = class WithFees extends ValidationStep {
   validate(ctx, session) {
     const [isValid, errors] = super.validate(ctx, session);
 
+    if (ctx.helpWithFeesAppliedForFees === 'No') {
+      delete ctx.helpWithFeesReferenceNumber;
+    }
+
     if (isValid) {
       // format reference number so it includes hyphens and HWF
       if (ctx.helpWithFeesReferenceNumber && ctx.helpWithFeesReferenceNumber.length) {

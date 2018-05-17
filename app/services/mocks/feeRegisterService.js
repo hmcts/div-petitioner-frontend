334800aaefb658fc2d0de3915d14d067ecd3a4cf
Rob Doherty <robertdoherty@hotmail.co.uk>
2018-05-17 14:42:34 +0100

    deleteme: test change

--- app/services/mocks/feeRegisterService.js
+++ app/services/mocks/feeRegisterService.js
@@ -2,7 +2,7 @@ const mockFeeResponse = {
   code: 'X0165',
   description: 'Filing an application for a divorce, nullity or civil partnership dissolution – fees order 1.2.',
   version: 1,
-  fee_amount: 550.00
+  fee_amount: 123.00
 };
 
 const get = () => {

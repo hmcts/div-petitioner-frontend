3dc0882192eb89348cc70345dfaac251ce622397
tony.chow <tony.chow@hmcts.net>
2018-05-23 15:32:28 +0100

    Fix feature toggle base path in outputs

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -3,7 +3,7 @@ output "idam_api_url" {
 }
 
 output "feature_toggle_api_url" {
-  value = "${var.feature_toggle_api_url}"
+  value = "${var.feature_toggle_api_url}${var.feature_toggle_api_base_path}"
 }
 
 output "case_progression_service_draft_url" {

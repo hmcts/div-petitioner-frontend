7d5473d3d0c9a8de5d91d43da5a321cc81e93369
tony.chow <tony.chow@hmcts.net>
2018-05-23 13:46:43 +0100

    Set proxy to whitespace string so it doesn't use the default value. Set outputs for certain envs required by the functional test script

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,3 +1,11 @@
 output "idam_api_url" {
   value = "${var.idam_api_url}"
 }
+
+output "feature_toggle_api_url" {
+  value = "${var.feature_toggle_api_url}"
+}
+
+output "case_progression_service_draft_url" {
+  value = "${var.case_progression_service_draft_url}"
+}

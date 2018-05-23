465911f9d961599dbae7b2888821a7c60ac3717a
tony.chow <tony.chow@hmcts.net>
2018-05-23 13:53:37 +0100

    Fixed draft store output url

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -7,5 +7,5 @@ output "feature_toggle_api_url" {
 }
 
 output "case_progression_service_draft_url" {
-  value = "${var.case_progression_service_draft_url}"
+  value = "${local.case_progression_service_url}${var.draft_store_api_base_path}"
 }

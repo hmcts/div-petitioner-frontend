9c11d81012ee32783058d20fe13b6ee99b2ebc1e
Rob Doherty <robertdoherty@hotmail.co.uk>
2018-05-25 14:10:55 +0100

    e2e runner url overrides

--- bin/run-functional-tests.sh
+++ bin/run-functional-tests.sh
@@ -22,7 +22,6 @@ export E2E_PROXY_BYPASS=${E2E_PROXY_BYPASS:-"*beta*LB.reform.hmcts.net"}
 export E2E_FRONTEND_NODE_ENV=${E2E_FRONTEND_NODE_ENV:-"production"}
 export IDAM_API_URL=${IDAM_API_URL:-"https://preprod-idamapi.reform.hmcts.net:3511"}
 export FEATURE_TOGGLE_API_URL=${FEATURE_TOGGLE_API_URL:-"http://betaPreProddivorceAppLB.reform.hmcts.net:4002/api/v1/feature-toggle"}
-export CASE_PROGRESSION_SERVICE_DRAFT_URL=${CASE_PROGRESSION_SERVICE_DRAFT_URL:-"http://div-case-progression-aat.service.core-compute-aat.internal/draftsapi/version/1"}
 export COURT_PHONENUMBER=${COURT_PHONENUMBER:-"0300 303 0642"}
 export COURT_OPENINGHOURS=${COURT_OPENINGHOURS:-"Monday to Friday, 8.30am to 5pm"}
 export COURT_EMAIL=${COURT_EMAIL:-"Divorce_Reform_Pro@Justice.gov.uk"}

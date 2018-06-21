a16e93e405ced63610d7316e5e42fd50ef28df54
John O'Neill <j.oneill@kainos.com>
2018-06-21 16:46:15 +0100

    DIV-2658: Awaiting payment and rejected checks on case status

--- app/middleware/submissionMiddleware.js
+++ app/middleware/submissionMiddleware.js
@@ -1,10 +1,16 @@
 const hasSubmitted = function(req, res, next) {
   const session = req.session;
 
-  if (!this.enabledAfterSubmission && session.caseId) { // eslint-disable-line
-    return res.redirect('/application-submitted');
+  if (!this.enabledAfterSubmission && session.caseId) {
+    switch (session.status) {
+    case 'Awaiting Payment':
+      return res.redirect('/application-submitted');
+    case 'Rejected':
+      return next();
+    default:
+      return res.redirect('/application-submitted-awaiting-response');
+    }
   }
-
   return next();
 };
 

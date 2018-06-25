7447ab64301161dad5a20803922bd639435f2562
John O'Neill <j.oneill@kainos.com>
2018-06-25 15:00:41 +0100

    Div-2658: Changed session.status to session.state

--- app/middleware/submissionMiddleware.test.js
+++ app/middleware/submissionMiddleware.test.js
@@ -18,21 +18,21 @@ describe(modulePath, () => {
     });
     it('redirects to /application-submitted if application has been submitted and is in "AwaitingPayment"', () => {
       req.session.caseId = 'someid';
-      req.session.status = 'AwaitingPayment';
+      req.session.state = 'AwaitingPayment';
       underTest.hasSubmitted.apply(ctx, [req, res, next]);
       expect(res.redirect.calledOnce).to.eql(true);
       expect(res.redirect.calledWith('/application-submitted')).to.eql(true);
     });
     it('redirects to /application-submitted-awaiting-response if application has been submitted and is not "AwaitingPayment" or "Rejected"', () => {
       req.session.caseId = 'someid';
-      req.session.status = 'redundantstatus';
+      req.session.state = 'redundantstatus';
       underTest.hasSubmitted.apply(ctx, [req, res, next]);
       expect(res.redirect.calledOnce).to.eql(true);
       expect(res.redirect.calledWith('/application-submitted-awaiting-response')).to.eql(true);
     });
-    it('calls next if application has been submitted and is "rejected"', () => {
+    it('calls next if application has been submitted and is "Rejected"', () => {
       req.session.caseId = 'someid';
-      req.session.status = 'Rejected';
+      req.session.state = 'Rejected';
       underTest.hasSubmitted.apply(ctx, [req, res, next]);
       expect(res.redirect.called).to.eql(false);
       expect(next.calledOnce).to.eql(true);

1f2a3fe7f8e8af18222b03d593919f81bed6d544
John O'Neill <j.oneill@kainos.com>
2018-06-27 14:36:45 +0100

    Updated to reference "case_state" instead of "state"

--- app/middleware/submissionMiddleware.js
+++ app/middleware/submissionMiddleware.js
@@ -2,7 +2,7 @@ const hasSubmitted = function(req, res, next) {
   const session = req.session;
 
   if (!this.enabledAfterSubmission && session.caseId) { // eslint-disable-line
-    switch (session.stateId) {
+    switch (session.case_state) {
     case 'AwaitingPayment':
       return res.redirect('/application-submitted');
     case 'Rejected':

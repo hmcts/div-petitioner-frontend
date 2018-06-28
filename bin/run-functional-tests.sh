69bd93d476e8800ec3180e5cd37cc294fb7dd16f
Rob Doherty <robertdoherty@hotmail.co.uk>
2018-06-28 14:37:50 +0100

    undo unnecessary commit

--- bin/run-functional-tests.sh
+++ bin/run-functional-tests.sh
@@ -3,10 +3,10 @@ set -ex
 
 # Setup required environment variables. TEST_URL should be set by CNP
 export E2E_FRONTEND_URL=${TEST_URL}
-export E2E_FRONTEND_NODE_ENV=${E2E_FRONTEND_NODE_ENV:-"production"}
-export IDAM_API_URL=${IDAM_API_URL:-"https://preprod-idamapi.reform.hmcts.net:3511"}
 export E2E_PROXY_SERVER=${E2E_PROXY_SERVER:-"proxyout.reform.hmcts.net:8080"}
 export E2E_PROXY_BYPASS=${E2E_PROXY_BYPASS:-"*beta*LB.reform.hmcts.net"}
+export E2E_FRONTEND_NODE_ENV=${E2E_FRONTEND_NODE_ENV:-"production"}
+export IDAM_API_URL=${IDAM_API_URL:-"https://preprod-idamapi.reform.hmcts.net:3511"}
 export COURT_PHONENUMBER=${COURT_PHONENUMBER:-"0300 303 0642"}
 export COURT_OPENINGHOURS=${COURT_OPENINGHOURS:-"Monday to Friday, 8.30am to 5pm"}
 export COURT_EMAIL=${COURT_EMAIL:-"divorce@justice.gov.uk"}

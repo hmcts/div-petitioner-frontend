9aecf770753fbc9819ce931ca78ab941cf8cdcff
Rob Doherty <robertdoherty@hotmail.co.uk>
2018-06-28 15:27:45 +0100

    reconfigured the idam e2e helper to only use a proxy when running from local

--- bin/run-functional-tests-local.sh
+++ bin/run-functional-tests-local.sh
@@ -4,6 +4,6 @@ set -ex
 # Extra default details needed during a local test run against CNP environment
 export E2E_FRONTEND_URL=${TEST_URL}
 export FEATURE_IDAM=${FEATURE_IDAM:-"true"}
-export E2E_PROXY_SERVER=${E2E_PROXY_SERVER:-"socks5://localhost:9000"}
+export E2E_IDAM_PROXY=${E2E_IDAM_PROXY:-"socks5://localhost:9000"}
 
 ./bin/run-functional-tests.sh

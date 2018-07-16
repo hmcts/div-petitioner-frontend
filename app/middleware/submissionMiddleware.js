29276f2c6ab506cd6374ddca427e366e15b724b3
Dominic Taylor <dom.taylor7@gmail.com>
2018-07-16 10:26:50 +0100

    restrict has submitted by prod

--- app/middleware/submissionMiddleware.js
+++ app/middleware/submissionMiddleware.js
@@ -1,7 +1,11 @@
+const config = require('config');
+
 const hasSubmitted = function(req, res, next) {
   const session = req.session;
 
-  if (!this.enabledAfterSubmission && session.caseId) { // eslint-disable-line
+  const hasSubmittedEnabled = ['prod'].includes(config.deployment_env);
+
+  if (hasSubmittedEnabled && !this.enabledAfterSubmission && session.caseId) { // eslint-disable-line
     switch (session.state) {
     case 'AwaitingPayment':
       return res.redirect('/application-submitted');

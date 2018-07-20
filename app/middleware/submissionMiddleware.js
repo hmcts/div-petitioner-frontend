8f7abcc3296cd830a0c6c099b405c665325709a1
John O'Neill <j.oneill@kainos.com>
2018-07-20 15:20:46 +0100

    potential-fix

--- app/middleware/submissionMiddleware.js
+++ app/middleware/submissionMiddleware.js
@@ -1,7 +1,7 @@
 const hasSubmitted = function(req, res, next) {
   const session = req.session;
 
-  if (!this.enabledAfterSubmission && session.caseId) { // eslint-disable-line
+  if (!this.enabledAfterSubmission && session.caseId && session.state) { // eslint-disable-line
     switch (session.state) {
     case 'AwaitingPayment':
       return res.redirect('/application-submitted');

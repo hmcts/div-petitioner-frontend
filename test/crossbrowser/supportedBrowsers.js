23bc69456031b0ea37c4f227857ae301e6f8bf29
Rob Doherty <robertdoherty@hotmail.co.uk>
2018-07-24 13:42:58 +0100

    update crossbrowser tests to work correctly with latest CNP overnight pipeline

--- test/crossbrowser/supportedBrowsers.js
+++ test/crossbrowser/supportedBrowsers.js
@@ -2,7 +2,7 @@ const supportedBrowsers = {
   microsoft: {
     ie11_win7: {
       browserName: 'internet explorer',
-      name: 'IE11_Win7',
+      name: 'DIV_IE11_Win7',
       platform: 'Windows 7',
       ignoreZoomSetting: true,
       nativeEvents: false,
@@ -11,7 +11,7 @@ const supportedBrowsers = {
     },
     // 'ie11_win8.1': {
     //   browserName: 'internet explorer',
-    //   name: 'IE11_Win8.1',
+    //   name: 'DIV_IE11_Win8.1',
     //   platform: 'Windows 8.1',
     //   ignoreZoomSetting: true,
     //   nativeEvents: false,
@@ -20,7 +20,7 @@ const supportedBrowsers = {
     // },
     ie11_win10: {
       browserName: 'internet explorer',
-      name: 'IE11_Win10',
+      name: 'DIV_IE11_Win10',
       platform: 'Windows 10',
       ignoreZoomSetting: true,
       nativeEvents: false,
@@ -29,13 +29,13 @@ const supportedBrowsers = {
     }
     // edge_latest: {
     //   browserName: 'MicrosoftEdge',
-    //   name: 'IEEdge_LATEST',
+    //   name: 'DIV_IEEdge_LATEST',
     //   platform: 'Windows 10',
     //   version: 'latest'
     // },
     // edge_previous: {
     //   browserName: 'MicrosoftEdge',
-    //   name: 'IEEdge_PREVIOUS',
+    //   name: 'DIV_IEEdge_PREVIOUS',
     //   platform: 'Windows 10',
     //   version: 'latest-1'
     // },
@@ -43,25 +43,25 @@ const supportedBrowsers = {
   chrome: {
     chrome_win_latest: {
       browserName: 'chrome',
-      name: 'WIN_CHROME_LATEST',
+      name: 'DIV_WIN_CHROME_LATEST',
       platform: 'Windows 10',
       version: 'latest'
     },
     chrome_win_previous: {
       browserName: 'chrome',
-      name: 'WIN_CHROME_PREVIOUS',
+      name: 'DIV_WIN_CHROME_PREVIOUS',
       platform: 'Windows 8.1',
       version: 'latest-1'
     },
     chrome_mac_latest: {
       browserName: 'chrome',
-      name: 'MAC_CHROME_LATEST',
+      name: 'DIV_MAC_CHROME_LATEST',
       platform: 'OS X 10.12',
       version: 'latest'
     },
     chrome_mac_previous: {
       browserName: 'chrome',
-      name: 'MAC_CHROME_PREVIOUS',
+      name: 'DIV_MAC_CHROME_PREVIOUS',
       platform: 'OS X 10.11',
       version: 'latest-1'
     }
@@ -69,28 +69,28 @@ const supportedBrowsers = {
   firefox: {
     firefox_win_latest: {
       browserName: 'firefox',
-      name: 'WIN_FIREFOX_LATEST',
+      name: 'DIV_WIN_FIREFOX_LATEST',
       platform: 'Windows 10',
       version: 'latest',
       'moz:webdriverClick': false
     },
     firefox_win_previous: {
       browserName: 'firefox',
-      name: 'WIN_FIREFOX_PREVIOUS',
+      name: 'DIV_WIN_FIREFOX_PREVIOUS',
       platform: 'Windows 8.1',
       version: 'latest-1',
       'moz:webdriverClick': false
     },
     firefox_mac_latest: {
       browserName: 'firefox',
-      name: 'MAC_FIREFOX_LATEST',
+      name: 'DIV_MAC_FIREFOX_LATEST',
       platform: 'OS X 10.12',
       version: 'latest',
       'moz:webdriverClick': false
     },
     firefox_mac_previous: {
       browserName: 'firefox',
-      name: 'MAC_FIREFOX_PREVIOUS',
+      name: 'DIV_MAC_FIREFOX_PREVIOUS',
       platform: 'OS X 10.11',
       version: 'latest-1',
       'moz:webdriverClick': false
@@ -99,14 +99,14 @@ const supportedBrowsers = {
   // safari: {
   // safari10: {
   //   browserName: 'safari',
-  //   name: 'SAFARI_10',
+  //   name: 'DIV_SAFARI_10',
   //   platform: 'macOS 10.12',
   //   version: '10.1',
   //   avoidProxy: true
   // },
   // safari11: {
   //   browserName: 'safari',
-  //   name: 'SAFARI_11',
+  //   name: 'DIV_SAFARI_11',
   //   platform: 'macOS 10.13',
   //   version: '11.1',
   //   avoidProxy: true
@@ -118,7 +118,7 @@ const supportedBrowsers = {
       appiumVersion: '1.8.1',
       deviceName: 'iPhone 6s Simulator',
       deviceOrientation: 'portrait',
-      name: 'IPHONE6S_IOS10',
+      name: 'DIV_IPHONE6S_IOS10',
       platformVersion: '10.3',
       platformName: 'iOS'
     }
@@ -127,7 +127,7 @@ const supportedBrowsers = {
     //   appiumVersion: '1.8.1',
     //   deviceName: 'iPhone 8 Simulator',
     //   deviceOrientation: 'portrait',
-    //   name: 'IPHONE8_IOS11',
+    //   name: 'DIV_IPHONE8_IOS11',
     //   platformVersion: '11.3',
     //   platformName: 'iOS'
     // },
@@ -138,7 +138,7 @@ const supportedBrowsers = {
       appiumVersion: '1.8.1',
       deviceName: 'Samsung Galaxy S8 GoogleAPI Emulator',
       deviceOrientation: 'portrait',
-      name: 'SAMSUNGS8_ANDROID7',
+      name: 'DIV_SAMSUNGS8_ANDROID7',
       platformVersion: '7.1',
       platformName: 'Android'
     }
@@ -147,7 +147,7 @@ const supportedBrowsers = {
     //   appiumVersion: '1.8.1',
     //   deviceName: 'Google Nexus 7C Emulator',
     //   deviceOrientation: 'portrait',
-    //   name: 'NEXUS7C_ANDROID4',
+    //   name: 'DIV_NEXUS7C_ANDROID4',
     //   platformVersion: '4.4',
     //   platformName: 'Android'
     // }

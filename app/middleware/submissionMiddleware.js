890fb441960b0e050b2988baea247e487363b23d
Warren Taggart <warren.taggart@hmcts.net>
2018-07-27 15:05:31 +0100

    Testing

--- app/middleware/submissionMiddleware.js
+++ app/middleware/submissionMiddleware.js
@@ -1,11 +1,8 @@
-const config = require('config');
 
 const hasSubmitted = function(req, res, next) {
   const session = req.session;
 
-  const hasSubmittedEnabled = ['prod'].includes(config.deployment_env);
-
-  if (hasSubmittedEnabled && !this.enabledAfterSubmission && session.caseId) { // eslint-disable-line
+  if (!this.enabledAfterSubmission && session.caseId) { // eslint-disable-line
     switch (session.state) {
     case 'AwaitingPayment':
       return res.redirect('/application-submitted');

e7386f9f076113cb02f67798e7f3f8dbc8d0896e
niyioyelade <oyeniyi.oyelade>
2018-08-18 03:32:45 +0100

    DIV-3151-Applicant is able to create Case without specifying Petitioner and Respondent Names - adding another test case

--- app/core/helpers/removeEmptyValues.test.js
+++ app/core/helpers/removeEmptyValues.test.js
@@ -10,11 +10,13 @@ describe(modulePath, () => {
       value1: 'value1',
       value2: '',
       value3: ' ',
-      value4: '   '
+      value4: '   ',
+      value5: true
     };
     const newCtx = removeEmptyValues(ctx);
     expect(newCtx.hasOwnProperty('value2')).to.eql(false);
     expect(newCtx.hasOwnProperty('value3')).to.eql(false);
     expect(newCtx.hasOwnProperty('value4')).to.eql(false);
+    expect(newCtx.hasOwnProperty('value5')).to.eql(true);
   });
 });

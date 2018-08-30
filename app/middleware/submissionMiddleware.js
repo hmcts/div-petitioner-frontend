40ecf8348535b91bc1d0b7a8c3cca57e8a752487
Tony Chow <36265570+tchow8@users.noreply.github.com>
2018-08-30 12:40:17 +0100

    Update submissionMiddleware.js

--- app/middleware/submissionMiddleware.js
+++ app/middleware/submissionMiddleware.js
@@ -4,10 +4,18 @@ const parseBool = require('app/core/utils/parseBool');
 const APPLICATION_SUBMITTED_PATH = '/application-submitted';
 const APPLICATION_AWAITING_RESPONSE_PATH = '/application-submitted-awaiting-response';
 
+const logger = require('app/services/logger').logger(__filename);
+
 const hasSubmitted = function(req, res, next) {
   const session = req.session;
 
   const hasSubmittedEnabled = ['prod'].includes(config.deployment_env);
+  
+  logger.info("Begin Submission logging");
+  logger.info(hasSubmittedEnabled);
+  logger.info(config.features.redirectToApplicationSubmitted);
+  logger.info(session.caseId);
+  logger.info(parseBool(config.features.redirectToApplicationSubmitted));
 
   // when an existing case is found and we have the state
   if (hasSubmittedEnabled && session.caseId && session.state) { // eslint-disable-line

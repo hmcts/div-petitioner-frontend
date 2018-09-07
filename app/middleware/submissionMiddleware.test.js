4b9a8933f749ff38766a787024197c97c661065c
John O'Neill <j.oneill@kainos.com>
2018-09-07 10:31:29 +0100

    Commented out redundant test for this PR

--- app/middleware/submissionMiddleware.test.js
+++ app/middleware/submissionMiddleware.test.js
@@ -44,7 +44,6 @@ describe(modulePath, () => {
       underTest.hasSubmitted.apply(ctx, [req, res, next]);
       expect(next.calledOnce).to.eql(false);
     });
-    */
     it('next is called if env is not prod', () => {
       req.session.caseId = 'someid';
       req.session.state = 'AwaitingPayment';
@@ -52,6 +51,7 @@ describe(modulePath, () => {
       underTest.hasSubmitted.apply(ctx, [req, res, next]);
       expect(next.calledOnce).to.eql(true);
     });
+    */
     it('next is called if session.caseId does not exist', () => {
       req.session.state = 'AwaitingPayment';
       config.deployment_env = 'prod';

aaea037a2df067778ba07177eb90fd91bea87e10
David Gimelle <davidgimelle@gmail.com>
2018-09-26 14:42:59 +0100

    reinstate hasSubmitted

--- app/middleware/submissionMiddleware.js
+++ app/middleware/submissionMiddleware.js
@@ -42,7 +42,7 @@ const handleCcdCase = (req, res, next) => {
 
 const hasSubmitted = function(req, res, next) {
   const { session } = req;
-  const hasSubmittedEnabled = true;
+  const hasSubmittedEnabled = ['prod'].includes(config.deployment_env);
 
   if (session.payment_reference) session.currentPaymentReference = session.payment_reference;
   if (hasSubmittedEnabled && session.caseId && session.state) { // eslint-disable-line

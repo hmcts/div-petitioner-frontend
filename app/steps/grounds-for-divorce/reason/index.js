5c2bf056e571d731cde39a00ecfcfd58f59f1b07
Dominic Taylor <dom.taylor7@gmail.com>
2019-01-17 11:20:50 +0000

    Petitioner journey - 5 year separation

--- app/steps/grounds-for-divorce/reason/index.js
+++ app/steps/grounds-for-divorce/reason/index.js
@@ -19,7 +19,7 @@ module.exports = class ReasonForDivorce extends ValidationStep {
         'unreasonable-behaviour': this.steps.UnreasonableBehaviour,
         adultery: this.steps.AdulteryWishToName,
         'separation-2-years': parseBool(config.features.respondentConsent) ? this.steps.RespondentConsent : this.steps.SeparationDate,
-        'separation-5-years': this.steps.SeparationDate,
+        'separation-5-years': parseBool(config.features.release510) ? this.steps.SeparationDateNew : this.steps.SeparationDate,
         desertion: this.steps.DesertionDate
       }
     };

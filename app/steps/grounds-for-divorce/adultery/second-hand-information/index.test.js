5192bb28323478130725f3bcb43c7f7eb776068d
Ravinder Pal Singh <ravinderpal96@live.it>
2019-01-21 12:18:07 +0000

    unit tests

---
+++ app/steps/grounds-for-divorce/adultery/second-hand-information/index.test.js
@@ -0,0 +1,152 @@
+const server = require('app');
+const request = require('supertest');
+const idamMock = require('test/mocks/idam');
+const {
+  testContent, testErrors, testRedirect,
+  testCYATemplate, testExistenceCYA,
+  postData, expectSessionValue
+} = require('test/util/assertions');
+const { removeStaleData } = require('app/core/helpers/staleDataManager');
+const { withSession } = require('test/util/setup');
+const { expect } = require('test/util/chai');
+const { clone } = require('lodash');
+
+const modulePath = 'app/steps/grounds-for-divorce/adultery/second-hand-information';
+
+const content = require(`${modulePath}/content`);
+
+let s = {};
+let agent = {};
+let underTest = {};
+
+describe(modulePath, () => {
+  beforeEach(() => {
+    idamMock.stub();
+    s = server.init();
+    agent = request.agent(s.app);
+    underTest = s.steps.AdulterySecondHandInfo;
+  });
+
+  afterEach(() => {
+    idamMock.restore();
+  });
+
+  describe('success', () => {
+    let session = {};
+
+    beforeEach(done => {
+      session = { divorceWho: 'wife' };
+      withSession(done, agent, session);
+    });
+
+    it('renders the content from the content file', done => {
+      testContent(done, agent, underTest, content);
+    });
+
+    it('renders errors for missing required context', done => {
+      const context = { reasonForDivorceAdulterySecondHandInfo: 'Yes' };
+      const onlyKey = [ 'reasonForDivorceAdulterySecondHandInfoDetails'];
+      testErrors(done, agent, underTest, context, content, 'required', onlyKey);
+    });
+
+    it('redirects to the next page when the user received information from another person', done => {
+      const context = {
+        reasonForDivorceAdulterySecondHandInfo: 'Yes',
+        reasonForDivorceAdulterySecondHandInfoDetails: 'placeholder'
+      };
+
+      testRedirect(done, agent, underTest, context, s.steps.LegalProceedings);
+    });
+
+    it('redirects to the next page when user did not receive information from another person', done => {
+      const context = { reasonForDivorceAdulterySecondHandInfo: 'No' };
+
+      testRedirect(done, agent, underTest, context, s.steps.LegalProceedings);
+    });
+  });
+
+  describe('setting details on session', () => {
+    beforeEach(done => {
+      const session = { divorceWho: 'wife' };
+      withSession(done, agent, session);
+    });
+
+    it('does not modify second-hand-info details when users has them', done => {
+      const context = {
+        reasonForDivorceAdulterySecondHandInfo: 'Yes',
+        reasonForDivorceAdulterySecondHandInfoDetails: 'placeholder'
+      };
+
+      postData(agent, underTest.url, context).then(
+        expectSessionValue(
+          'reasonForDivorceAdulterySecondHandInfoDetails',
+          'placeholder',
+          agent,
+          done
+        )
+      );
+    });
+
+    it('sets the second-hand-info details when user does not have second-hand-info details', done => {
+      const context = { reasonForDivorceAdulterySecondHandInfo: 'No' };
+
+      postData(agent, underTest.url, context).then(
+        expectSessionValue(
+          'reasonForDivorceAdulterySecondHandInfoDetails',
+          undefined, // eslint-disable-line no-undefined
+          agent,
+          done
+        )
+      );
+    });
+  });
+
+  describe('Watch session values', () => {
+    it('removes reasonForDivorceAdulterySecondHandInfoDetails if reasonForDivorceAdulterySecondHandInfo is set to No', () => {
+      const previousSession = {
+        reasonForDivorceAdulterySecondHandInfo: 'Yes',
+        reasonForDivorceAdulterySecondHandInfoDetails: 'placeholder'
+      };
+
+      const session = clone(previousSession);
+      session.reasonForDivorceAdulterySecondHandInfo = 'No';
+
+      const newSession = removeStaleData(previousSession, session);
+      expect(newSession.reasonForDivorceAdulterySecondHandInfo).to.equal('No');
+      expect(typeof newSession.reasonForDivorceAdulterySecondHandInfoDetails)
+        .to.equal('undefined');
+    });
+
+    it('removes reasonForDivorceAdulterySecondHandInfo and reasonForDivorceAdulterySecondHandInfoDetails if reasonForDivorce is not Adultery', () => {
+      const previousSession = {
+        reasonForDivorceAdulterySecondHandInfo: 'Yes',
+        reasonForDivorceAdulterySecondHandInfoDetails: 'placeholder'
+      };
+
+      const session = clone(previousSession);
+      session.reasonForDivorce = 'Behaviour';
+
+      const newSession = removeStaleData(previousSession, session);
+      expect(typeof newSession.reasonForDivorceAdulterySecondHandInfo).to.equal('undefined');
+      expect(typeof newSession.reasonForDivorceAdulterySecondHandInfoDetails)
+        .to.equal('undefined');
+    });
+  });
+
+  describe('Check Your Answers', () => {
+    it('renders the cya template', done => {
+      testCYATemplate(done, underTest);
+    });
+
+    it('renders adultery second-hand-information details', done => {
+      const contentToExist = [ 'question' ];
+
+      const valuesToExist = ['reasonForDivorceAdulterySecondHandInfo'];
+
+      const context = { reasonForDivorceAdulterySecondHandInfo: 'Yes' };
+      // also add the info details?
+      testExistenceCYA(done, underTest, content,
+        contentToExist, valuesToExist, context);
+    });
+  });
+});

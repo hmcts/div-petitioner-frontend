e39c562bfd4dabe868739c1670c511c0d088da9d
Mounika <mounika.ammineni@hmcts.net>
2019-01-29 07:37:10 +0000

    tests added

--- app/services/mocks/feesAndPaymentsService.js
+++ app/services/mocks/feesAndPaymentsService.js
@@ -1,22 +1,70 @@
-const mockFeeResponse = {
-  feeCode: 'FEE0002',
-  version: 4,
-  amount: 550.00
+const feeTypes = {
+  applicationFee: 'petition-issue-fee',
+  amendFee: 'amend-fee',
+  defendPetitionFee: 'defended-petition-fee',
+  generalAppFee: 'general-application-fee',
+  enforcementFee: 'enforcement-fee',
+  appFinancialOrderFee: 'application-financial-order-fee',
+  appWithoutNoticeFee: 'application-without-notice-fee'
 };
+
+const mockFeeResponse = (feeType = '') => {
+  switch (feeType) {
+  case feeTypes.amendFee:
+    return {
+      feeCode: 'FEE0269',
+      version: 1,
+      amount: 95
+    };
+  case feeTypes.defendPetitionFee:
+    return {
+      feeCode: 'FEE0307',
+      version: 1,
+      amount: 50
+    };
+  case feeTypes.generalAppFee:
+    return {
+      feeCode: 'FEE0428',
+      version: 1,
+      amount: 110
+    };
+  case feeTypes.appFinancialOrderFee:
+    return {
+      feeCode: 'FEE0643',
+      amount: 255,
+      version: 1
+    };
+  case feeTypes.appWithoutNoticeFee:
+    return {
+      feeCode: 'FEE0640',
+      version: 1,
+      amount: 50
+    };
+  case feeTypes.enforcementFee:
+    return {
+      feeCode: 'FEE0424',
+      version: 1,
+      amount: 245
+    };
+  default:
+    return {
+      feeCode: 'FEE0002',
+      version: 4,
+      amount: 550
+    };
+  }
+};
+
 
 const get = () => {
   return new Promise(resolve => {
-    resolve(mockFeeResponse);
+    resolve(mockFeeResponse());
   });
 };
 
 const getFee = feeType => {
   return new Promise(resolve => {
-    resolve({
-      feeCode: feeType,
-      version: 1,
-      amount: 95.00
-    });
+    resolve(mockFeeResponse(feeType));
   });
 };
 

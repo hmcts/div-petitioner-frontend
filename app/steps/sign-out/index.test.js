e63655e7206d5870e3dce1715001423c134b5d63
tony.chow <tony.chow@hmcts.net>
2019-01-30 14:36:52 +0000

    Fixed unit tests for sign out

--- app/steps/sign-out/index.test.js
+++ app/steps/sign-out/index.test.js
@@ -1,26 +1,57 @@
-const { expect, sinon } = require('test/util/chai');
+/* eslint-disable max-nested-callbacks, no-undefined */
+const request = require('supertest');
+const clone = require('lodash').cloneDeep;
+const { expect } = require('test/util/chai');
+const { withSession } = require('test/util/setup');
+const { mockSession } = require('test/fixtures');
+const { expectSessionValue, postData, testRedirect } = require('test/util/assertions');
+const idamMock = require('test/mocks/idam');
+const server = require('app');
 
 const modulePath = 'app/steps/sign-out';
-const UnderTest = require(modulePath);
+
+let s = {};
+let agent = {};
+let underTest = {};
 
 describe(modulePath, () => {
-  const req = {};
-  const res = {};
-  let step = {};
-  let next = {};
-
   beforeEach(() => {
-    res.redirect = sinon.spy();
-    step = new UnderTest();
-    next = sinon.stub();
+    idamMock.stub();
+    s = server.init();
+    agent = request.agent(s.app);
+    underTest = s.steps.SignOut;
   });
 
-  it('redirects to index page', () => {
-    // Act.
-    step.handler(req, res, next);
-    // Assert.
-    expect(next.calledOnce).to.equal(true);
-    expect(res.redirect.calledOnce).to.equal(true);
-    expect(res.redirect.calledWith('/index')).to.equal(true);
+  afterEach(() => {
+    idamMock.restore();
+  });
+
+  describe('success', () => {
+    beforeEach(done => {
+      const session = clone(mockSession);
+      withSession(done, agent, session);
+    });
+
+    it('should immediately redirect to the index page and clear the session', done => {
+      const context = {};
+
+      testRedirect(done, agent, underTest, context,
+        s.steps.Index);
+    });
+
+    it('should clear the session', done => {
+      const context = {};
+
+      agent.get('/session').then(response => {
+        expect(response.body.divorceWho).to.not.equal(undefined);
+
+        postData(agent, underTest.url, context).then(() => {
+          expectSessionValue('divorceWho', undefined, agent, done)();
+        });
+      })
+        .catch(error => {
+          done(error);
+        });
+    });
   });
 });

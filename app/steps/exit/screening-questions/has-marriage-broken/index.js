6e2949d2d6ad0c106b31581c30c3b7f819d18603
tony.chow <tony.chow@hmcts.net>
2019-01-31 14:18:07 +0000

    Changed screening question exit steps to ExitStep class

--- app/steps/exit/screening-questions/has-marriage-broken/index.js
+++ app/steps/exit/screening-questions/has-marriage-broken/index.js
@@ -1,6 +1,6 @@
-const Step = require('app/core/steps/Step');
+const ExitStep = require('app/core/steps/ExitStep');
 
-module.exports = class ExitMarriageBroken extends Step {
+module.exports = class ExitMarriageBroken extends ExitStep {
   get url() {
     return '/exit/screening-questions/marriage-broken';
   }

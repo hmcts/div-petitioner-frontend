01714dae0c497ac9fd3159de319d952817a8660a
Mounika <mounika.ammineni@hmcts.net>
2019-01-31 11:18:16 +0000

    Tests fixed

--- app/steps/grounds-for-divorce/lived-apart-since/index.test.js
+++ app/steps/grounds-for-divorce/lived-apart-since/index.test.js
@@ -76,8 +76,8 @@ describe(modulePath, () => {
     it('render calculated values', done => {
       const valuesToTest = [
         '8 weeks',
-        '5 days',
-        '61 days',
+        '6 days',
+        '62 days',
         session.reasonForDivorceLivingApartDate.format('DD MMMM YYYY')
       ];
       testMultipleValuesExistence(

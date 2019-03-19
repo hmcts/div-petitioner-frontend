a6f9b95d7790fe5b52e5b1d1424970f2f77775e4
vivek reddy <30834324+vianvi@users.noreply.github.com>
2019-03-19 16:18:22 +0000

    Add region specific to run nightly tests on European data centre (#478)

--- test/end-to-end/saucelabs.conf.js
+++ test/end-to-end/saucelabs.conf.js
@@ -38,6 +38,7 @@ const setupConfig = {
       cssSelectorsEnabled: 'true',
       host: 'ondemand.saucelabs.com',
       port: 80,
+      region: 'eu',
       user: process.env.SAUCE_USERNAME || CONF.saucelabs.username,
       key: process.env.SAUCE_ACCESS_KEY || CONF.saucelabs.key,
       desiredCapabilities: {}

44daf2baa70bf4faabc5deed941d522d6adf3f8b
J. Victor Ferreira <victor.ferreira@hmcts.net>
2019-04-04 16:42:10 +0100

    Unit tests passing

--- app/middleware/checkCookiesAllowed.js
+++ app/middleware/checkCookiesAllowed.js
@@ -1,11 +1,25 @@
-function checkCookiesAllowed(req, res, next) {
-  const requiredCookiesAreSet = req.cookies['connect.sid'];
+const attemptToSetTestCookieParam = 'attemptToSetTestCookie';
+const testCookieName = 'test';
 
-  if (requiredCookiesAreSet) {
+function checkCookiesAllowed(req, res, next) {
+  const cookies = req.cookies;
+
+  if (Object.keys(cookies).length) {
+    // Remove test cookie, if it exists
+    res.clearCookie('test');
+
     next();
+  } else if (req.query[attemptToSetTestCookieParam]) {
+    // This is an attempt to set a test cookie
+
+    if (!cookies.test) {
+      // Client does not accept cookies. Show them error page
+      res.redirect('/cookie-error');
+    }
   } else {
-    //  cookie could not be set
-    res.redirect('/cookie-error');
+    // Cookie was not found, we'll attempt to set a test cookie
+    res.cookie(testCookieName, 'testCookie');
+    res.redirect(`${req.baseUrl}?${attemptToSetTestCookieParam}=true`);
   }
 }
 

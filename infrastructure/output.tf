a7c4a14a53cd30d9ec1ec5af4ba6779da376bf25
John O'Neill <j.oneill@kainos.com>
2019-04-09 17:30:51 +0100

    DIV-4569: Replaced 530 feature toggle (#495)

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -21,7 +21,3 @@ output "feature_release_520" {
 output "feature_release_520_desertion" {
   value = "${var.feature_release_520_desertion}"
 }
-
-output "feature_release_530" {
-  value = "${var.feature_release_530}"
-}

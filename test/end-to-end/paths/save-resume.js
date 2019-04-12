74d4724765bc8c2eb9a0368e375f5acfe05ba382
Tony Chow <tony.chow@hmcts.net>
2019-04-12 12:24:02 +0100

    Only run delete scenario

--- test/end-to-end/paths/save-resume.js
+++ test/end-to-end/paths/save-resume.js
@@ -1,81 +1,81 @@
 const CONF = require('config');
-const idamConfigHelper = require('test/end-to-end/helpers/idamConfigHelper.js');
+// const idamConfigHelper = require('test/end-to-end/helpers/idamConfigHelper.js');
 const parseBool = require('app/core/utils/parseBool');
 
 Feature('Draft petition store').retry(3);
 
-Scenario('See the check your answers page if session restored from draft petition store', function (I) {
-  I.amOnLoadedPage('/index');
+// Scenario('See the check your answers page if session restored from draft petition store', function (I) {
+//   I.amOnLoadedPage('/index');
 
-  if (parseBool(CONF.features.idam)) {
-    I.startApplication();
-    I.haveBrokenMarriage();
-    I.haveRespondentAddress();
-    I.haveMarriageCert();
-    I.selectHelpWithFees();
-    I.enterHelpWithFees();
-    I.selectDivorceType();
-    I.enterMarriageDate();
-    I.selectMarriedInUk();
-    I.clearCookie();
+//   if (parseBool(CONF.features.idam)) {
+//     I.startApplication();
+//     I.haveBrokenMarriage();
+//     I.haveRespondentAddress();
+//     I.haveMarriageCert();
+//     I.selectHelpWithFees();
+//     I.enterHelpWithFees();
+//     I.selectDivorceType();
+//     I.enterMarriageDate();
+//     I.selectMarriedInUk();
+//     I.clearCookie();
 
-    I.amOnLoadedPage('/index');
-  } else {
-    I.setCookie({name: 'mockRestoreSession', value: 'true'});
-    I.seeCookie('mockRestoreSession');
-  }
+//     I.amOnLoadedPage('/index');
+//   } else {
+//     I.setCookie({name: 'mockRestoreSession', value: 'true'});
+//     I.seeCookie('mockRestoreSession');
+//   }
 
-  I.startApplication();
+//   I.startApplication();
 
-  I.checkMyAnswersRestoredSession();
+//   I.checkMyAnswersRestoredSession();
 
-  I.seeCurrentUrlEquals('/jurisdiction/habitual-residence');
-});
+//   I.seeCurrentUrlEquals('/jurisdiction/habitual-residence');
+// });
 
-xScenario('See next unanswered question if toNextUnansweredPage=true in query string and session restored from draft petition store', async function (I) {
-  let cookie = '';
-  I.amOnLoadedPage('/index');
+// xScenario('See next unanswered question if toNextUnansweredPage=true in query string and session restored from draft petition store', async function (I) {
+//   let cookie = '';
+//   I.amOnLoadedPage('/index');
 
-  if (parseBool(CONF.features.idam)) {
-    I.startApplication();
-    I.haveBrokenMarriage();
-    I.haveRespondentAddress();
-    I.haveMarriageCert();
-    I.selectHelpWithFees();
-    I.enterHelpWithFees();
-    I.selectDivorceType();
-    I.enterMarriageDate();
-    I.selectMarriedInUk();
-    cookie = await I.grabCookie('__auth-token');
-    I.clearCookie();
-    I.wait(2);
-  } else {
-    I.setCookie({name: 'mockRestoreSession', value: 'true'});
-    I.seeCookie('mockRestoreSession');
-  }
+//   if (parseBool(CONF.features.idam)) {
+//     I.startApplication();
+//     I.haveBrokenMarriage();
+//     I.haveRespondentAddress();
+//     I.haveMarriageCert();
+//     I.selectHelpWithFees();
+//     I.enterHelpWithFees();
+//     I.selectDivorceType();
+//     I.enterMarriageDate();
+//     I.selectMarriedInUk();
+//     cookie = await I.grabCookie('__auth-token');
+//     I.clearCookie();
+//     I.wait(2);
+//   } else {
+//     I.setCookie({name: 'mockRestoreSession', value: 'true'});
+//     I.seeCookie('mockRestoreSession');
+//   }
 
-  I.amOnLoadedPage(`/authenticated?toNextUnansweredPage=true&__auth-token=${cookie}`);
+//   I.amOnLoadedPage(`/authenticated?toNextUnansweredPage=true&__auth-token=${cookie}`);
 
-  I.seeCurrentUrlEquals('/jurisdiction/habitual-residence');
-});
+//   I.seeCurrentUrlEquals('/jurisdiction/habitual-residence');
+// });
 
-Scenario('Save and close', function (I) {
-  I.amOnLoadedPage('/index');
+// Scenario('Save and close', function (I) {
+//   I.amOnLoadedPage('/index');
 
-  I.startApplication();
+//   I.startApplication();
 
-  I.haveBrokenMarriage();
-  I.haveRespondentAddress();
-  I.haveMarriageCert();
-  I.selectHelpWithFees();
+//   I.haveBrokenMarriage();
+//   I.haveRespondentAddress();
+//   I.haveMarriageCert();
+//   I.selectHelpWithFees();
 
-  I.clickSaveAndCLose();
-  I.seeCurrentUrlEquals('/exit/application-saved');
+//   I.clickSaveAndCLose();
+//   I.seeCurrentUrlEquals('/exit/application-saved');
 
-  if (parseBool(CONF.features.idam)) {
-    I.see(idamConfigHelper.getTestEmail());
-  }
-});
+//   if (parseBool(CONF.features.idam)) {
+//     I.see(idamConfigHelper.getTestEmail());
+//   }
+// });
 
 Scenario('Delete application from draft petition store', function (I) {
   I.amOnLoadedPage('/index');
@@ -103,74 +103,74 @@ Scenario('Delete application from draft petition store', function (I) {
   I.seeCurrentUrlEquals('/screening-questions/has-marriage-broken');
 });
 
-Scenario('I delete my amend petition from draft store', function (I) {
-  I.amOnLoadedPage('/index');
+// Scenario('I delete my amend petition from draft store', function (I) {
+//   I.amOnLoadedPage('/index');
 
-  if (parseBool(CONF.features.idam)) {
-    I.startApplication();
-    I.haveBrokenMarriage();
-    I.haveRespondentAddress();
-    I.haveMarriageCert();
-    I.selectHelpWithFees();
-    I.clearCookie();
+//   if (parseBool(CONF.features.idam)) {
+//     I.startApplication();
+//     I.haveBrokenMarriage();
+//     I.haveRespondentAddress();
+//     I.haveMarriageCert();
+//     I.selectHelpWithFees();
+//     I.clearCookie();
 
-    I.amOnLoadedPage('/index');
-  } else {
-    I.setCookie({name: 'mockRestoreSession', value: 'true'});
-    I.seeCookie('mockRestoreSession');
-  }
+//     I.amOnLoadedPage('/index');
+//   } else {
+//     I.setCookie({name: 'mockRestoreSession', value: 'true'});
+//     I.seeCookie('mockRestoreSession');
+//   }
 
-  I.startApplicationWith('amendPetitionSession');
-  I.checkMyAnswersRemoveApplication();
-  I.confirmRemoveApplication();
-  I.seeCurrentUrlEquals('/exit/removed-saved-application');
-});
+//   I.startApplicationWith('amendPetitionSession');
+//   I.checkMyAnswersRemoveApplication();
+//   I.confirmRemoveApplication();
+//   I.seeCurrentUrlEquals('/exit/removed-saved-application');
+// });
 
-Scenario('I do not delete my amend petition from draft store', function (I) {
-  I.amOnLoadedPage('/index');
+// Scenario('I do not delete my amend petition from draft store', function (I) {
+//   I.amOnLoadedPage('/index');
 
-  if (parseBool(CONF.features.idam)) {
-    I.startApplication();
-    I.haveBrokenMarriage();
-    I.haveRespondentAddress();
-    I.haveMarriageCert();
-    I.selectHelpWithFees();
-    I.clearCookie();
+//   if (parseBool(CONF.features.idam)) {
+//     I.startApplication();
+//     I.haveBrokenMarriage();
+//     I.haveRespondentAddress();
+//     I.haveMarriageCert();
+//     I.selectHelpWithFees();
+//     I.clearCookie();
 
-    I.amOnLoadedPage('/index');
-  } else {
-    I.setCookie({name: 'mockRestoreSession', value: 'true'});
-    I.seeCookie('mockRestoreSession');
-  }
+//     I.amOnLoadedPage('/index');
+//   } else {
+//     I.setCookie({name: 'mockRestoreSession', value: 'true'});
+//     I.seeCookie('mockRestoreSession');
+//   }
 
-  I.startApplicationWith('amendPetitionSession');
-  I.checkMyAnswersRemoveApplication();
-  I.declineRemoveApplicaiton();
+//   I.startApplicationWith('amendPetitionSession');
+//   I.checkMyAnswersRemoveApplication();
+//   I.declineRemoveApplicaiton();
 
-  I.seeCurrentUrlEquals('/check-your-answers');
-});
+//   I.seeCurrentUrlEquals('/check-your-answers');
+// });
 
-Scenario('Decline to delete application from draft petition store', function (I) {
-  I.amOnLoadedPage('/index');
+// Scenario('Decline to delete application from draft petition store', function (I) {
+//   I.amOnLoadedPage('/index');
 
-  if (parseBool(CONF.features.idam)) {
-    I.startApplication();
-    I.haveBrokenMarriage();
-    I.haveRespondentAddress();
-    I.haveMarriageCert();
-    I.selectHelpWithFees();
-    I.clearCookie();
+//   if (parseBool(CONF.features.idam)) {
+//     I.startApplication();
+//     I.haveBrokenMarriage();
+//     I.haveRespondentAddress();
+//     I.haveMarriageCert();
+//     I.selectHelpWithFees();
+//     I.clearCookie();
 
-    I.amOnLoadedPage('/index');
-  } else {
-    I.setCookie({name: 'mockRestoreSession', value: 'true'});
-    I.seeCookie('mockRestoreSession');
-  }
+//     I.amOnLoadedPage('/index');
+//   } else {
+//     I.setCookie({name: 'mockRestoreSession', value: 'true'});
+//     I.seeCookie('mockRestoreSession');
+//   }
 
-  I.startApplication();
+//   I.startApplication();
 
-  I.checkMyAnswersRemoveApplication();
-  I.declineRemoveApplicaiton();
+//   I.checkMyAnswersRemoveApplication();
+//   I.declineRemoveApplicaiton();
 
-  I.seeCurrentUrlEquals('/check-your-answers');
-});
+//   I.seeCurrentUrlEquals('/check-your-answers');
+// });

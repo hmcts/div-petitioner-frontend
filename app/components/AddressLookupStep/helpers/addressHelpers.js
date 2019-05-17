7a9643907d71376c36f2bc4bf13d9a17f69621f0
Chris Iliev <c.iliev@kainos.com>
2019-05-17 16:46:16 +0100

    WIP 2

--- app/components/AddressLookupStep/helpers/addressHelpers.js
+++ app/components/AddressLookupStep/helpers/addressHelpers.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const cleanLine = function (line) {
     return line.replace(' null', ' ').replace('null ', ' ')
         .replace(/undefined/g, '')
@@ -8,26 +9,26 @@ const cleanLine = function (line) {
 
 
 const buildAddressBaseUk = function (selectedAddress) {
-    let line1 = `${selectedAddress.DPA.ORGANISATION_NAME} ${selectedAddress.DPA.DEPARTMENT_NAME} ${selectedAddress.DPA.PO_BOX_NUMBER}`;
-    let line2 = `${selectedAddress.DPA.BUILDING_NAME} ${selectedAddress.DPA.SUB_BUILDING_NAME} ${selectedAddress.DPA.BUILDING_NUMBER} ${selectedAddress.DPA.THOROUGHFARE_NAME}`;
-    let line3 = `${selectedAddress.DPA.DEPENDENT_LOCALITY} ${selectedAddress.DPA.DOUBLE_DEPENDENT_LOCALITY} ${selectedAddress.DPA.DEPENDENT_THOROUGHFARE_NAME}`;
+  let line1 = `${selectedAddress.DPA.ORGANISATION_NAME} ${selectedAddress.DPA.DEPARTMENT_NAME} ${selectedAddress.DPA.PO_BOX_NUMBER}`;
+  let line2 = `${selectedAddress.DPA.BUILDING_NAME} ${selectedAddress.DPA.SUB_BUILDING_NAME} ${selectedAddress.DPA.BUILDING_NUMBER} ${selectedAddress.DPA.THOROUGHFARE_NAME}`;
+  let line3 = `${selectedAddress.DPA.DEPENDENT_LOCALITY} ${selectedAddress.DPA.DOUBLE_DEPENDENT_LOCALITY} ${selectedAddress.DPA.DEPENDENT_THOROUGHFARE_NAME}`;
 
-    if (cleanLine(line1).length === 0) {
-        line1 = line2;
-        line2 = line3;
-        line3 = '';
-    }
+  if (cleanLine(line1).length === 0) {
+    line1 = line2;
+    line2 = line3;
+    line3 = '';
+  }
 
-    const addressBaseUK = {
-        addressLine1: cleanLine(line1),
-        addressLine2: cleanLine(line2),
-        addressLine3: cleanLine(line3),
-        postCode: selectedAddress.DPA.POSTCODE,
-        postTown: selectedAddress.DPA.POST_TOWN,
-        county: '',
-        country: 'UK'
-    };
-    return addressBaseUK;
+  const addressBaseUK = {
+    addressLine1: cleanLine(line1),
+    addressLine2: cleanLine(line2),
+    addressLine3: cleanLine(line3),
+    postCode: selectedAddress.DPA.POSTCODE,
+    postTown: selectedAddress.DPA.POST_TOWN,
+    county: '',
+    country: 'UK'
+  };
+  return addressBaseUK;
 };
 
 // refactored based off logic in Royal Mail Programmers' Guide
@@ -35,84 +36,80 @@ const buildAddressBaseUk = function (selectedAddress) {
 // @todo prepend the word 'PO BOX' in the cases where it applies
 
 const defaultAddressTemplate = function (address) {
-    return {
-        ORG_NAME: address.DPA.ORGANISATION_NAME,
-        DEPT_NAME: address.DPA.DEPARTMENT_NAME,
-        PO_BOX: address.DPA.PO_BOX_NUMBER,
-        SUB_BUILDING_NAME: address.DPA.SUB_BUILDING_NAME,
-        BUILDING_NAME: address.DPA.BUILDING_NAME,
-        BUILDING_NUMBER: address.DPA.BUILDING_NUMBER,
-        DEPENDENT_THOROUGHFARE_NAME: address.DPA.DEPENDENT_THOROUGHFARE_NAME,
-        THOROUGHFARE_NAME: address.DPA.THOROUGHFARE_NAME,
-        DOUBLE_DEPENDENT_LOCALITY: address.DPA.DOUBLE_DEPENDENT_LOCALITY,
-        DEPENDENT_LOCALITY: address.DPA.DEPENDENT_LOCALITY,
-    }
+  return {
+    ORG_NAME: address.DPA.ORGANISATION_NAME,
+    DEPT_NAME: address.DPA.DEPARTMENT_NAME,
+    PO_BOX: address.DPA.PO_BOX_NUMBER,
+    SUB_BUILDING_NAME: address.DPA.SUB_BUILDING_NAME,
+    BUILDING_NAME: address.DPA.BUILDING_NAME,
+    BUILDING_NUMBER: address.DPA.BUILDING_NUMBER,
+    DEPENDENT_THOROUGHFARE_NAME: address.DPA.DEPENDENT_THOROUGHFARE_NAME,
+    THOROUGHFARE_NAME: address.DPA.THOROUGHFARE_NAME,
+    DOUBLE_DEPENDENT_LOCALITY: address.DPA.DOUBLE_DEPENDENT_LOCALITY,
+    DEPENDENT_LOCALITY: address.DPA.DEPENDENT_LOCALITY,
+  }
 };
 
 
 function buildNameIsAnException(buildingName) {
-    firstChar = buildingName.charAt(0);
-    lastChar = buildingName.slice(-1);
-    penultimateChar = buildingName.slice(-2);
+  let firstChar = buildingName.charAt(0);
+  let lastChar = buildingName.slice(-1);
+  let penultimateChar = buildingName.charAt(-2);
 
-    if (!firstChar.isNaN() && !lastChar.isNaN()) {
-        return true;
-    }
-    if ((isNaN(lastChar) && !isNaN(penultimateChar) && !isNaN(firstChar))) {
-        return true;
-    }
-    return !!penultimateChar.match(/[a-z]/i);
+  // if first and last char are numbers
+  if (!isNaN(firstChar) && !isNaN(lastChar)) {
+    return true;
+  }
+  // if first and penultimate chars are numbers and the final one is a letter
+  if ((isNaN(lastChar) && !isNaN(penultimateChar) && !isNaN(firstChar))) {
+    return true;
+  }
+  // @todo check with rule for one/multiple characters
+  return Boolean(firstChar.match(/[a-z]/i));
 }
 
-const buildAddressV2 = function (templateAddress) {
-    let firstLine = '';
-    let secondLine = '';
-    const localitiesThoroughfares = `${templateAddress.DEPENDENT_THOROUGHFARE_NAME} 
+const buildConcatenatedAddress = function (address) {
+  const templateAddress = defaultAddressTemplate(address);
+  let firstLine = '';
+  let secondLine = '';
+  const localitiesThoroughfares = `${templateAddress.DEPENDENT_THOROUGHFARE_NAME} 
     ${templateAddress.THOROUGHFARE_NAME} 
     ${templateAddress.DOUBLE_DEPENDENT_LOCALITY} 
     ${templateAddress.DEPENDENT_LOCALITY}`;
 
-    // assume there's an organisation and dept name
-    let organisationLine = `${templateAddress.ORG_NAME} ${templateAddress.DEPT_NAME} ${templateAddress.PO_BOX}`;
+  // assume there's an organisation and dept name
+  const organisationLine = `${templateAddress.ORG_NAME} ${templateAddress.DEPT_NAME} ${templateAddress.PO_BOX}`;
 
-    if (templateAddress.BUILDING_NAME &&
-        buildNameIsAnException(templateAddress.BUILDING_NAME) &&
-        !templateAddress.BUILDING_NUMBER) {
-        // format acc to rule 3/6
-        firstLine = `${templateAddress.SUB_BUILDING_NAME}`;
-        secondLine = `${templateAddress.BUILDING_NAME} ` + localitiesThoroughfares;
-    } else {
-        firstLine = `${templateAddress.SUB_BUILDING_NAME} ${templateAddress.BUILDING_NAME}`;
-        secondLine = `${templateAddress.BUILDING_NUMBER} ` + localitiesThoroughfares;
-    }
-    // finally check for org/dept
-    if (templateAddress.ORG_NAME || templateAddress.DEPT_NAME) {
-        secondLine = firstLine + " " + secondLine;
-        firstLine = organisationLine;
-    }
+  // if building name is an exception and no building number
+  // format according to Rule 3/6
+  if (templateAddress.BUILDING_NAME && buildNameIsAnException(templateAddress.BUILDING_NAME) && !templateAddress.BUILDING_NUMBER) {
+    // Rule 3/6
+    firstLine = `${templateAddress.SUB_BUILDING_NAME}`;
+    secondLine = templateAddress.BUILDING_NAME + localitiesThoroughfares;
+  } else {
+    // default formatting
+    firstLine = `${templateAddress.SUB_BUILDING_NAME} ${templateAddress.BUILDING_NAME}`;
+    secondLine = templateAddress.BUILDING_NUMBER + localitiesThoroughfares
+  }
+  // finally check for org/dept names, which should appear on the first line and precede everything else
+  if (templateAddress.ORG_NAME || templateAddress.DEPT_NAME) {
+    secondLine = firstLine + secondLine;
+    firstLine = organisationLine;
+  }
 
-    let concatenatedAddress = [];
+  firstLine = cleanLine(firstLine);
+  secondLine = cleanLine(secondLine);
 
-    if (cleanLine(secondLine) === '') {
-        concatenatedAddress = [
-            cleanLine(firstLine),
-            address.DPA.POST_TOWN,
-            address.DPA.POSTCODE
+  // if first line is empty, replace it with the second line
+  if (cleanLine(firstLine).length === 0) {
+    firstLine = secondLine;
+    secondLine = '';
+  }
 
-        ];
-    } else {
-        concatenatedAddress = [
-            cleanLine(firstLine),
-            cleanLine(secondLine),
-            address.DPA.POST_TOWN,
-            address.DPA.POSTCODE
-
-        ];
-    }
-
-    return concatenatedAddress;
+  // only return first line if second line is empty
+  return (secondLine.length !== 0) ? [firstLine, secondLine, address.DPA.POST_TOWN, address.DPA.POSTCODE] :
+    [firstLine, address.DPA.POST_TOWN, address.DPA.POSTCODE];
 };
-
 
 // if (templateAddress.BUILDING_NAME &&
 //     buildNameIsAnException(templateAddress.BUILDING_NAME) &&
@@ -174,61 +171,59 @@ const buildAddressV2 = function (templateAddress) {
 
 // @todo Refactor this to reduce complexity.
 
-const buildConcatenatedAddress = function (address) { // eslint-disable-line complexity
-    let firstLine = `${address.DPA.ORGANISATION_NAME} ${address.DPA.DEPARTMENT_NAME} ${address.DPA.PO_BOX_NUMBER} ${address.DPA.SUB_BUILDING_NAME}  ${address.DPA.BUILDING_NUMBER}, ${address.DPA.THOROUGHFARE_NAME} ${address.DPA.BUILDING_NAME}`;
-    let secondLine = `${address.DPA.DEPENDENT_LOCALITY} ${address.DPA.DOUBLE_DEPENDENT_LOCALITY}  ${address.DPA.DEPENDENT_THOROUGHFARE_NAME} `;
+const buildConcatenatedAddress1 = function (address) { // eslint-disable-line complexity
+  let firstLine = `${address.DPA.ORGANISATION_NAME} ${address.DPA.DEPARTMENT_NAME} ${address.DPA.PO_BOX_NUMBER} ${address.DPA.SUB_BUILDING_NAME}  ${address.DPA.BUILDING_NUMBER}, ${address.DPA.THOROUGHFARE_NAME} ${address.DPA.BUILDING_NAME}`;
+  let secondLine = `${address.DPA.DEPENDENT_LOCALITY} ${address.DPA.DOUBLE_DEPENDENT_LOCALITY}  ${address.DPA.DEPENDENT_THOROUGHFARE_NAME} `;
 
-    if (`${address.DPA.BUILDING_NAME}` !== 'undefined') {
-        firstLine = `${address.DPA.ORGANISATION_NAME} ${address.DPA.DEPARTMENT_NAME} ${address.DPA.SUB_BUILDING_NAME} ${address.DPA.BUILDING_NUMBER}, ${address.DPA.BUILDING_NAME} `;
-        secondLine = `${address.DPA.DEPENDENT_LOCALITY} ${address.DPA.DOUBLE_DEPENDENT_LOCALITY} ${address.DPA.THOROUGHFARE_NAME} ${address.DPA.DEPENDENT_THOROUGHFARE_NAME} `;
-    }
+  if (`${address.DPA.BUILDING_NAME}` !== 'undefined') {
+    firstLine = `${address.DPA.ORGANISATION_NAME} ${address.DPA.DEPARTMENT_NAME} ${address.DPA.SUB_BUILDING_NAME} ${address.DPA.BUILDING_NUMBER}, ${address.DPA.BUILDING_NAME} `;
+    secondLine = `${address.DPA.DEPENDENT_LOCALITY} ${address.DPA.DOUBLE_DEPENDENT_LOCALITY} ${address.DPA.THOROUGHFARE_NAME} ${address.DPA.DEPENDENT_THOROUGHFARE_NAME} `;
+  }
 
-    if (`${address.DPA.BUILDING_NAME}` !== 'undefined' && `${address.DPA.THOROUGHFARE_NAME}` !== 'undefined' && `${address.DPA.BUILDING_NUMBER}` === 'undefined') {
-        firstLine = `${address.DPA.ORGANISATION_NAME} ${address.DPA.DEPARTMENT_NAME} ${address.DPA.SUB_BUILDING_NAME} ${address.DPA.BUILDING_NAME} `;
-        secondLine = `${address.DPA.DEPENDENT_LOCALITY} ${address.DPA.DOUBLE_DEPENDENT_LOCALITY} ${address.DPA.THOROUGHFARE_NAME} ${address.DPA.DEPENDENT_THOROUGHFARE_NAME} `;
-    }
+  if (`${address.DPA.BUILDING_NAME}` !== 'undefined' && `${address.DPA.THOROUGHFARE_NAME}` !== 'undefined' && `${address.DPA.BUILDING_NUMBER}` === 'undefined') {
+    firstLine = `${address.DPA.ORGANISATION_NAME} ${address.DPA.DEPARTMENT_NAME} ${address.DPA.SUB_BUILDING_NAME} ${address.DPA.BUILDING_NAME} `;
+    secondLine = `${address.DPA.DEPENDENT_LOCALITY} ${address.DPA.DOUBLE_DEPENDENT_LOCALITY} ${address.DPA.THOROUGHFARE_NAME} ${address.DPA.DEPENDENT_THOROUGHFARE_NAME} `;
+  }
 
-    if (`${address.DPA.PO_BOX_NUMBER}` !== 'undefined') {
-        firstLine = ` ${address.DPA.CLASSIFICATION_CODE_DESCRIPTION} ${address.DPA.PO_BOX_NUMBER}, ${address.DPA.ORGANISATION_NAME} ${address.DPA.DEPARTMENT_NAME}   ${address.DPA.SUB_BUILDING_NAME} ${address.DPA.BUILDING_NUMBER} ${address.DPA.BUILDING_NAME} `;
-        secondLine = `${address.DPA.DEPENDENT_LOCALITY} ${address.DPA.DOUBLE_DEPENDENT_LOCALITY} ${address.DPA.THOROUGHFARE_NAME} ${address.DPA.DEPENDENT_THOROUGHFARE_NAME} `;
-    }
+  if (`${address.DPA.PO_BOX_NUMBER}` !== 'undefined') {
+    firstLine = ` ${address.DPA.CLASSIFICATION_CODE_DESCRIPTION} ${address.DPA.PO_BOX_NUMBER}, ${address.DPA.ORGANISATION_NAME} ${address.DPA.DEPARTMENT_NAME}   ${address.DPA.SUB_BUILDING_NAME} ${address.DPA.BUILDING_NUMBER} ${address.DPA.BUILDING_NAME} `;
+    secondLine = `${address.DPA.DEPENDENT_LOCALITY} ${address.DPA.DOUBLE_DEPENDENT_LOCALITY} ${address.DPA.THOROUGHFARE_NAME} ${address.DPA.DEPENDENT_THOROUGHFARE_NAME} `;
+  }
 
-    if (`${address.DPA.BUILDING_NUMBER}` !== 'undefined' && `${address.DPA.SUB_BUILDING_NAME}` !== 'undefined' && `${address.DPA.THOROUGHFARE_NAME}` !== 'undefined') {
-        if (`${address.DPA.BUILDING_NAME}` === 'undefined') {
-            firstLine = `${address.DPA.ORGANISATION_NAME} ${address.DPA.DEPARTMENT_NAME} ${address.DPA.SUB_BUILDING_NAME}`;
-            secondLine = `${address.DPA.BUILDING_NUMBER}, ${address.DPA.DEPENDENT_THOROUGHFARE_NAME} ${address.DPA.THOROUGHFARE_NAME} ${address.DPA.DOUBLE_DEPENDENT_LOCALITY} ${address.DPA.DEPENDENT_LOCALITY} `;
-        } else {
-            firstLine = `${address.DPA.ORGANISATION_NAME} ${address.DPA.DEPARTMENT_NAME} ${address.DPA.SUB_BUILDING_NAME}, ${address.DPA.BUILDING_NAME} `;
-            secondLine = `${address.DPA.BUILDING_NUMBER}, ${address.DPA.DEPENDENT_THOROUGHFARE_NAME} ${address.DPA.THOROUGHFARE_NAME} ${address.DPA.DOUBLE_DEPENDENT_LOCALITY} ${address.DPA.DEPENDENT_LOCALITY} `;
-        }
-    }
-
-    if (`${address.DPA.ORGANISATION_NAME}` !== 'undefined' && `${address.DPA.THOROUGHFARE_NAME}` !== 'undefined') {
-        firstLine = `${address.DPA.ORGANISATION_NAME} ${address.DPA.DEPARTMENT_NAME}`;
-        secondLine = `${address.DPA.BUILDING_NUMBER} ${address.DPA.SUB_BUILDING_NAME} ${address.DPA.BUILDING_NAME}, ${address.DPA.THOROUGHFARE_NAME} ${address.DPA.DEPENDENT_THOROUGHFARE_NAME} ${address.DPA.DEPENDENT_LOCALITY} ${address.DPA.DOUBLE_DEPENDENT_LOCALITY}`;
-    }
-
-
-    let concatenatedAddress = [];
-
-    if (cleanLine(secondLine) === '') {
-        concatenatedAddress = [
-            cleanLine(firstLine),
-            address.DPA.POST_TOWN,
-            address.DPA.POSTCODE
-
-        ];
+  if (`${address.DPA.BUILDING_NUMBER}` !== 'undefined' && `${address.DPA.SUB_BUILDING_NAME}` !== 'undefined' && `${address.DPA.THOROUGHFARE_NAME}` !== 'undefined') {
+    if (`${address.DPA.BUILDING_NAME}` === 'undefined') {
+      firstLine = `${address.DPA.ORGANISATION_NAME} ${address.DPA.DEPARTMENT_NAME} ${address.DPA.SUB_BUILDING_NAME}`;
+      secondLine = `${address.DPA.BUILDING_NUMBER}, ${address.DPA.DEPENDENT_THOROUGHFARE_NAME} ${address.DPA.THOROUGHFARE_NAME} ${address.DPA.DOUBLE_DEPENDENT_LOCALITY} ${address.DPA.DEPENDENT_LOCALITY} `;
     } else {
-        concatenatedAddress = [
-            cleanLine(firstLine),
-            cleanLine(secondLine),
-            address.DPA.POST_TOWN,
-            address.DPA.POSTCODE
-
-        ];
+      firstLine = `${address.DPA.ORGANISATION_NAME} ${address.DPA.DEPARTMENT_NAME} ${address.DPA.SUB_BUILDING_NAME}, ${address.DPA.BUILDING_NAME} `;
+      secondLine = `${address.DPA.BUILDING_NUMBER}, ${address.DPA.DEPENDENT_THOROUGHFARE_NAME} ${address.DPA.THOROUGHFARE_NAME} ${address.DPA.DOUBLE_DEPENDENT_LOCALITY} ${address.DPA.DEPENDENT_LOCALITY} `;
     }
+  }
 
-    return concatenatedAddress;
+  if (`${address.DPA.ORGANISATION_NAME}` !== 'undefined' && `${address.DPA.THOROUGHFARE_NAME}` !== 'undefined') {
+    firstLine = `${address.DPA.ORGANISATION_NAME} ${address.DPA.DEPARTMENT_NAME}`;
+    secondLine = `${address.DPA.BUILDING_NUMBER} ${address.DPA.SUB_BUILDING_NAME} ${address.DPA.BUILDING_NAME}, ${address.DPA.THOROUGHFARE_NAME} ${address.DPA.DEPENDENT_THOROUGHFARE_NAME} ${address.DPA.DEPENDENT_LOCALITY} ${address.DPA.DOUBLE_DEPENDENT_LOCALITY}`;
+  }
+
+
+  let concatenatedAddress = [];
+
+  if (cleanLine(secondLine) === '') {
+    concatenatedAddress = [
+      cleanLine(firstLine),
+      address.DPA.POST_TOWN,
+      address.DPA.POSTCODE
+    ];
+  } else {
+    concatenatedAddress = [
+      cleanLine(firstLine),
+      cleanLine(secondLine),
+      address.DPA.POST_TOWN,
+      address.DPA.POSTCODE
+    ];
+  }
+
+  return concatenatedAddress;
 };
 
-module.exports = {buildAddressBaseUk, buildConcatenatedAddress};
+module.exports = { buildAddressBaseUk, buildConcatenatedAddress };

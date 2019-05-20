c2a931caacb5440d6365c83a2cec85c8ad5632c7
Chris Iliev <c.iliev@kainos.com>
2019-05-20 18:20:05 +0100

    Refactoring and added a third address line to match Royal Mail guidelines

--- app/components/AddressLookupStep/helpers/addressHelpers.js
+++ app/components/AddressLookupStep/helpers/addressHelpers.js
@@ -1,14 +1,13 @@
-/* eslint-disable */
-const cleanLine = function (line) {
-    return line.replace(' null', ' ').replace('null ', ' ')
-        .replace(/undefined/g, '')
-        .replace(/ +/g, ' ')
-        .trim()
-        .replace(/^,/g, '');
+const cleanLine = function(line) {
+  return line.replace(' null', ' ').replace('null ', ' ')
+    .replace(/undefined/g, '')
+    .replace(/\s\s+/g, ' ')
+    .trim()
+    .replace(/^,/g, '');
 };
 
 
-const buildAddressBaseUk = function (selectedAddress) {
+const buildAddressBaseUk = function(selectedAddress) {
   let line1 = `${selectedAddress.DPA.ORGANISATION_NAME} ${selectedAddress.DPA.DEPARTMENT_NAME} ${selectedAddress.DPA.PO_BOX_NUMBER}`;
   let line2 = `${selectedAddress.DPA.BUILDING_NAME} ${selectedAddress.DPA.SUB_BUILDING_NAME} ${selectedAddress.DPA.BUILDING_NUMBER} ${selectedAddress.DPA.THOROUGHFARE_NAME}`;
   let line3 = `${selectedAddress.DPA.DEPENDENT_LOCALITY} ${selectedAddress.DPA.DOUBLE_DEPENDENT_LOCALITY} ${selectedAddress.DPA.DEPENDENT_THOROUGHFARE_NAME}`;
@@ -35,7 +34,7 @@ const buildAddressBaseUk = function (selectedAddress) {
 // https://www.royalmail.com/sites/default/files/docs/pdf/programmers_guide_edition_7_v5.pdf
 // @todo prepend the word 'PO BOX' in the cases where it applies
 
-const defaultAddressTemplate = function (address) {
+const defaultAddressTemplate = function(address) {
   return {
     ORG_NAME: address.DPA.ORGANISATION_NAME,
     DEPT_NAME: address.DPA.DEPARTMENT_NAME,
@@ -46,70 +45,115 @@ const defaultAddressTemplate = function (address) {
     DEPENDENT_THOROUGHFARE_NAME: address.DPA.DEPENDENT_THOROUGHFARE_NAME,
     THOROUGHFARE_NAME: address.DPA.THOROUGHFARE_NAME,
     DOUBLE_DEPENDENT_LOCALITY: address.DPA.DOUBLE_DEPENDENT_LOCALITY,
-    DEPENDENT_LOCALITY: address.DPA.DEPENDENT_LOCALITY,
-  }
+    DEPENDENT_LOCALITY: address.DPA.DEPENDENT_LOCALITY
+  };
 };
 
+const orderedTfaresLocalities = templateAddress => {
+  // order which thoroughfares and localities should be displayed in
+  return [
+    templateAddress.DEPENDENT_THOROUGHFARE_NAME,
+    templateAddress.THOROUGHFARE_NAME,
+    templateAddress.DOUBLE_DEPENDENT_LOCALITY,
+    templateAddress.DEPENDENT_LOCALITY
+  ];
+};
 
 function buildNameIsAnException(buildingName) {
-  let firstChar = buildingName.charAt(0);
-  let lastChar = buildingName.slice(-1);
-  let penultimateChar = buildingName.charAt(-2);
+  if (!buildingName) {
+    return false;
+  }
 
-  // if first and last char are numbers
+  const firstChar = buildingName.charAt(0);
+  // Exception rule 3 - Building Name has only one character (eg ‘A’)
+  if (buildingName.length === 1) {
+    return Boolean(firstChar.match(/[a-z]/i));
+  }
+
+  const lastChar = buildingName.slice(-1);
+  // Exception rule 1 - first and last characters are numeric
   if (!isNaN(firstChar) && !isNaN(lastChar)) {
     return true;
   }
-  // if first and penultimate chars are numbers and the final one is a letter
-  if ((isNaN(lastChar) && !isNaN(penultimateChar) && !isNaN(firstChar))) {
-    return true;
-  }
-  // @todo check with rule for one/multiple characters
-  return Boolean(firstChar.match(/[a-z]/i));
+  // Exception rule 2 - First and penultimate characters are numeric, last character is alphabetic
+  const penultimateCharIndex = -2;
+  return (isNaN(lastChar) && !isNaN(buildingName.slice(penultimateCharIndex, -1)) && !isNaN(firstChar));
 }
 
-const buildConcatenatedAddress = function (address) {
+const splitTfaresLocalities = orderedTfaresLocalitiesFields => {
+  const tfaresLocalities = orderedTfaresLocalitiesFields.filter(Boolean);
+  const firstTfareLocalityElement = tfaresLocalities[0];
+  // leave only the subsequent thoroughfare/locality lines
+  tfaresLocalities.splice(0, 1);
+  return {
+    firstLine: `${firstTfareLocalityElement}`,
+    otherLines: tfaresLocalities.join(' ')
+  };
+};
+
+function buildAddressLines(address) {
   const templateAddress = defaultAddressTemplate(address);
   let firstLine = '';
   let secondLine = '';
-  const localitiesThoroughfares = `${templateAddress.DEPENDENT_THOROUGHFARE_NAME} 
-    ${templateAddress.THOROUGHFARE_NAME} 
-    ${templateAddress.DOUBLE_DEPENDENT_LOCALITY} 
-    ${templateAddress.DEPENDENT_LOCALITY}`;
+  let thirdLine = '';
 
-  // assume there's an organisation and dept name
+  const thoroughfaresLocalities = splitTfaresLocalities(
+    orderedTfaresLocalities(templateAddress));
+
+  // build organisation, dept name and po box line - should come first per Rule 1
   const organisationLine = `${templateAddress.ORG_NAME} ${templateAddress.DEPT_NAME} ${templateAddress.PO_BOX}`;
 
-  // if building name is an exception and no building number
-  // format according to Rule 3/6
-  if (templateAddress.BUILDING_NAME && buildNameIsAnException(templateAddress.BUILDING_NAME) && !templateAddress.BUILDING_NUMBER) {
-    // Rule 3/6
-    firstLine = `${templateAddress.SUB_BUILDING_NAME}`;
-    secondLine = templateAddress.BUILDING_NAME + localitiesThoroughfares;
+  // Rules 3 and 6 - if building name/sub-building name is an exception and no building number
+  if (buildNameIsAnException(templateAddress.BUILDING_NAME) && !templateAddress.BUILDING_NUMBER) {
+    // sub-building name and building name on separate lines when sub-building name not an exception OR does not exist
+    if (buildNameIsAnException(templateAddress.SUB_BUILDING_NAME) === false) {
+      firstLine = `${templateAddress.SUB_BUILDING_NAME}`;
+      secondLine = `${templateAddress.BUILDING_NAME} ${thoroughfaresLocalities.firstLine}`;
+      thirdLine = `${thoroughfaresLocalities.otherLines}`;
+    }
+    // when sub-building is an exception - same line as building name
+    firstLine = `${templateAddress.SUB_BUILDING_NAME} ${templateAddress.BUILDING_NAME} ${thoroughfaresLocalities.firstLine}`;
+    secondLine = `${thoroughfaresLocalities.otherLines}`;
   } else {
-    // default formatting
-    firstLine = `${templateAddress.SUB_BUILDING_NAME} ${templateAddress.BUILDING_NAME}`;
-    secondLine = templateAddress.BUILDING_NUMBER + localitiesThoroughfares
+    // given a/no building number and no/exceptional/non-exceptional building name
+    if (buildNameIsAnException(templateAddress.SUB_BUILDING_NAME)) {
+      // if there's a sub-building name and it's an exception - Rules 6 and 7
+      firstLine = `${templateAddress.SUB_BUILDING_NAME} ${templateAddress.BUILDING_NAME}`;
+      secondLine = `${templateAddress.BUILDING_NUMBER} ${thoroughfaresLocalities.firstLine}`;
+      thirdLine = `${thoroughfaresLocalities.otherLines}`;
+    }
+    // sub-building name is not an exception OR doesn't exist - Rules 6 and 7
+    firstLine = `${templateAddress.SUB_BUILDING_NAME}`;
+    secondLine = `${templateAddress.BUILDING_NAME}`;
+    thirdLine = `${templateAddress.BUILDING_NUMBER} ${thoroughfaresLocalities.firstLine} ${thoroughfaresLocalities.otherLines}`;
   }
-  // finally check for org/dept names, which should appear on the first line and precede everything else
+  // Org/dept names should appear on the first line and precede everything else - Rule 1
   if (templateAddress.ORG_NAME || templateAddress.DEPT_NAME) {
-    secondLine = firstLine + secondLine;
+    secondLine = `${firstLine} ${secondLine}`;
     firstLine = organisationLine;
   }
 
-  firstLine = cleanLine(firstLine);
-  secondLine = cleanLine(secondLine);
+  return {
+    firstAddressLine: firstLine,
+    secondAddressLine: secondLine,
+    thirdAddressLine: thirdLine
+  };
+}
 
-  // if first line is empty, replace it with the second line
-  if (cleanLine(firstLine).length === 0) {
-    firstLine = secondLine;
-    secondLine = '';
-  }
+function buildConcatenatedAddress(address) {
+  const allAddressLines = Object.values(buildAddressLines(address));
 
-  // only return first line if second line is empty
-  return (secondLine.length !== 0) ? [firstLine, secondLine, address.DPA.POST_TOWN, address.DPA.POSTCODE] :
-    [firstLine, address.DPA.POST_TOWN, address.DPA.POSTCODE];
-};
+  const addressLines = allAddressLines.reduce((acc, current) => {
+    const line = cleanLine(current);
+    if (line) {
+      acc.push(line);
+    }
+    return acc;
+  }, []);
+
+  addressLines.push(address.DPA.POST_TOWN, address.DPA.POSTCODE);
+  return addressLines;
+}
 
 // if (templateAddress.BUILDING_NAME &&
 //     buildNameIsAnException(templateAddress.BUILDING_NAME) &&
@@ -171,7 +215,8 @@ const buildConcatenatedAddress = function (address) {
 
 // @todo Refactor this to reduce complexity.
 
-const buildConcatenatedAddress1 = function (address) { // eslint-disable-line complexity
+// eslint-disable-next-line no-unused-vars
+const buildConcatenatedAddress1 = function(address) { // eslint-disable-line complexity
   let firstLine = `${address.DPA.ORGANISATION_NAME} ${address.DPA.DEPARTMENT_NAME} ${address.DPA.PO_BOX_NUMBER} ${address.DPA.SUB_BUILDING_NAME}  ${address.DPA.BUILDING_NUMBER}, ${address.DPA.THOROUGHFARE_NAME} ${address.DPA.BUILDING_NAME}`;
   let secondLine = `${address.DPA.DEPENDENT_LOCALITY} ${address.DPA.DOUBLE_DEPENDENT_LOCALITY}  ${address.DPA.DEPENDENT_THOROUGHFARE_NAME} `;
 

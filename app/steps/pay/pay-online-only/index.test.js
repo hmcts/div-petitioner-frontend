75fb8d13ab2cdace443974bb829ab8f29c40b9eb
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2019-05-22 11:18:16 +0100

    Update feeToBePaid if it is not available in session (#544)

--- app/steps/pay/pay-online-only/index.test.js
+++ app/steps/pay/pay-online-only/index.test.js
@@ -60,9 +60,26 @@ describe(modulePath, () => {
     });
   });
 
-  describe('success', () => {
+
+  describe('New Application', () => {
     it('renders the content from the content file', done => {
-      testContent(done, agent, underTest, content);
+      const dataContent = { feeToBePaid: '550' };
+      testContent(done, agent, underTest, content, dataContent);
+    });
+  });
+
+  describe('Amended Case', () => {
+    let session = {};
+
+    beforeEach(done => {
+      session = { previousCaseId: 'old-case-id' };
+
+      withSession(done, agent, session);
+    });
+
+    it('renders the content from the content file', done => {
+      const dataContent = { feeToBePaid: '95' };
+      testContent(done, agent, underTest, content, dataContent);
     });
   });
 

545b1d1c5f2f88598228ab9332ca1238eb9d3cb8
tony.chow <tony.chow@hmcts.net>
2019-08-08 14:32:49 +0100

    Remove awaits when not required

--- test/end-to-end/helpers/JSWait.js
+++ test/end-to-end/helpers/JSWait.js
@@ -14,7 +14,7 @@ class JSWait extends codecept_helper {
     const helper = this.helpers['WebDriverIO'] || this.helpers['Puppeteer'];
     const helperIsPuppeteer = this.helpers['Puppeteer'];
 
-    await helper.click(text, locator).catch(err => { console.error(err.message); });
+    helper.click(text, locator).catch(err => { console.error(err.message); });
 
     if (helperIsPuppeteer) {
       await helper.page.waitForNavigation({waitUntil: 'networkidle0'});
@@ -32,7 +32,7 @@ class JSWait extends codecept_helper {
         url = helper.options.url + url;
       }
 
-      await helper.page.goto(url).catch(err => { console.error(err.message); });
+      helper.page.goto(url).catch(err => { console.error(err.message); });
       await helper.page.waitForNavigation({waitUntil: 'networkidle0'});
 
     } else {

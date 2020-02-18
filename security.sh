bf82e043c24a0adc721aff095c853e4923893545
vianvi <30834324+vianvi@users.noreply.github.com>
2020-02-18 14:58:49 +0000

    update Known Issues in suppression file

--- security.sh
+++ security.sh
@@ -21,17 +21,9 @@ while !(curl -s http://0.0.0.0:1001) > /dev/null
   cp *.html functional-output/
   cp activescanReport.xml functional-output/
 
-if [ -f zapKnownIssues.xml ]; then
-
-egrep -v "<uri>|<solution>|<otherinfo>|generated.*>" zapKnownIssues.xml > zapKnownIssuesUpdated.xml
-egrep -v "<uri>|<solution>|<otherinfo>|generated.*>" functional-output/activescanReport.xml > functional-output/activescanReportUpated.xml
-
-  if diff -q zapKnownIssuesUpdated.xml functional-output/activescanReportUpated.xml --ignore-all-space > output.xml 2>&1; then
-    echo
-    echo Ignorning known vulnerabilities
-    exit 0
-  fi
-fi
+  zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Low --exit-code False
+  curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
+  cp *.* functional-output/
 
 echo
 echo ZAP Security vulnerabilities were found that were not ignored
@@ -42,8 +34,6 @@ echo fixes and they do not apply to production, you may ignore them
 echo
 echo To ignore these vulnerabilities, add them to:
 echo
-echo "./zapKnownIssues.xml"
+echo "./audit.json"
 echo
 echo and commit the change
-
-zap-cli -p 1001 alerts -l Medium

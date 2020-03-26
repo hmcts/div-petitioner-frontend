792954b0ce778b6df0b8f56f036959977514176f
Luca Del Buono <luca.delbuono@hmcts.net>
2020-03-26 11:13:33 +0000

    testing...

--- app/middleware/commonContent.js
+++ app/middleware/commonContent.js
@@ -1,23 +1,20 @@
 const i18next = require('i18next').createInstance();
+const common = require('app/content/common-en');
+const commonJurisdiction = require('app/content/commonJurisdiction-en');
 const { merge } = require('lodash');
 const logger = require('app/services/logger').logger(__filename);
 const CONF = require('config');
 
+const content = merge(common, commonJurisdiction);
+
+i18next.init(content, error => {
+  if (error) {
+    logger.errorWithReq(null, 'i18next_error', 'Failed to initialise i18next', error.message);
+  }
+});
+i18next.changeLanguage('en');
+
 function commonContentMiddleware(req, res, next) {
-  const common = require(`app/content/common-${req.session.language}`);
-  const commonJurisdiction = require(`app/content/commonJurisdiction-${req.session.language}`);
-
-  const content = merge(common, commonJurisdiction);
-
-  i18next.init(content, error => {
-    if (error) {
-      logger.errorWithReq(null, 'i18next_error', 'Failed to initialise i18next', error.message);
-    }
-  });
-
-  i18next.languages = CONF.languages;
-  i18next.changeLanguage(req.session.language);
-
   const i18nProxy = new Proxy(i18next, {
     get: (target, key) => {
       if (target.exists(key)) {
@@ -37,7 +34,6 @@ function commonContentMiddleware(req, res, next) {
   res.locals.i18n = i18next;
   res.locals.common = i18nProxy;
   res.locals.content = i18nProxy;
-
   next();
 }
 

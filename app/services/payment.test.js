cd42a8274bdf740d8ede64ce057907b13ed5ee74
Luca Del Buono <luca.delbuono@hmcts.net>
2020-05-07 11:12:59 +0100

    WLA-497: fix eslint

--- app/services/payment.test.js
+++ app/services/payment.test.js
@@ -261,11 +261,7 @@ describe(modulePath, () => {
 
   describe('#queryAllPayments', () => {
     let client = null;
-    const req = {
-      session: {
-        language: 'cy'
-      }
-    };
+    const req = { session: { language: 'cy' } };
     const user = { bearerToken: '123' };
     const serviceToken = 'someServiceToken';
 

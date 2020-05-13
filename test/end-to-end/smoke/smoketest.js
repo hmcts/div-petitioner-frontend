cf84ee5867d089264f063aecfa12a95539e895fe
Luca Del Buono <luca.delbuono@hmcts.net>
2020-05-13 17:59:49 +0100

    WLA-491: Put back smoke test steps

--- test/end-to-end/smoke/smoketest.js
+++ test/end-to-end/smoke/smoketest.js
@@ -1,6 +1,6 @@
 Feature('Smoke test', { retries: 2 });
 
-// Scenario('Can see frontend index page', (I) => {
-//   I.amOnLoadedPage('/index');
-//   I.see('Sign in');
-// });
+Scenario('Can see frontend index page', (I) => {
+  I.amOnLoadedPage('/index');
+  I.see('Sign in');
+});

719c15968dcff42c2671ec762ad9d29f0af86b47
Luca Del Buono <luca.delbuono@hmcts.net>
2020-05-13 17:47:24 +0100

    WLA-491: Put back smoke test steps

--- test/end-to-end/paths/startSession.js
+++ test/end-to-end/paths/startSession.js
@@ -3,6 +3,6 @@ Feature('Initiating Session').retry(3);
 Scenario('Redirected to /index page when request a session-required page with no session cookies', (I) => {
 
   I.clearCookie();
-  // I.amOnLoadedPage('/check-your-answers');
-  // I.see('Sign in');
+  I.amOnLoadedPage('/check-your-answers');
+  I.see('Sign in');
 });

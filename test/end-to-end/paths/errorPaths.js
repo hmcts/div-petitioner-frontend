4fca979ce7c889509def76c5fe8620561f51d9ed
Luca <luca.delbuono@hmcts.com>
2020-05-28 11:58:35 +0100

    WLA-93: Re-enable cookie error e2e

--- test/end-to-end/paths/errorPaths.js
+++ test/end-to-end/paths/errorPaths.js
@@ -11,23 +11,13 @@ Scenario('Incorrect URLs are served a 404 page', (I) => {
 
 });
 
-Scenario('Redirects to login page or cookie error page based on build if start application and clear cookies', async (I) => {
+Scenario('Redirects to cookie error page if start application with no cookies', async (I) => {
   I.amOnLoadedPage('/index');
   I.startApplication();
   I.clearCookie();
   //This simulates a situation where the browser has no cookies even after the middleware tried to set one for testing whether the browser accepts cookies
   I.amOnLoadedPage('/authenticated?attemptToSetTestCookie=true');
-
-  // let previewUrl = await I.grabCurrentUrl();
-  // let splitPath = previewUrl.split('-')[5];
-  // let urlContainsPreview = splitPath.split('.');
-
-  // if(urlContainsPreview[0] === 'preview'){
-  //   I.seeCurrentUrlEquals('/cookie-error');
-  // }
-  // else{
-  //   I.seeInCurrentUrl('/login?');
-  // }
+  I.seeCurrentUrlEquals('/cookie-error');
 });
 
 Scenario('check cookie error page exists', (I) => {

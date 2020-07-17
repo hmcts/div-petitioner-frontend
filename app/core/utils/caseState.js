725e4ac7481b6fd2d6fd5633989267b353b183e1
Felix Eyetan <peyetan@yahoo.com>
2020-07-17 07:33:26 +0100

    Minor code refactoring to fix build

--- app/core/utils/caseState.js
+++ app/core/utils/caseState.js
@@ -1,8 +1,6 @@
 const { isEmpty, isEqual, get } = require('lodash');
 
-const caseStates = {
-  AwaitingAmendCase: 'AwaitingAmendCase'
-};
+const awaitingAmendCase = 'AwaitingAmendCase';
 
 const isToggleOn = session => {
   const toggle = get(session, 'featureToggles', { ft_awaiting_amend: false });
@@ -13,13 +11,11 @@ const isAwaitingAmendCase = sessionData => {
   if (!isToggleOn(sessionData)) {
     return false;
   }
-
-  const state = get(sessionData, 'state');
-  if (isEmpty(state)) {
+  const currentState = get(sessionData, 'state');
+  if (isEmpty(currentState)) {
     return false;
   }
-
-  return isEqual(state, caseStates.AwaitingAmendCase);
+  return isEqual(currentState, awaitingAmendCase);
 };
 
 module.exports = {

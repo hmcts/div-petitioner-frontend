1f79311062806e583542d68e1f61cf87319e62b8
mseyeni@icloud.com <marc-steevene@Ilya.lan>
2020-07-31 15:00:49 +0100

    Redirect to language preference going through index

--- app/steps/amendment-explanatory-page/index.test.js
+++ app/steps/amendment-explanatory-page/index.test.js
@@ -255,10 +255,10 @@ describe(modulePath, () => {
       });
     });
 
-    it('when continue button is clicked should request amend and redirect to next unanswered page', done => {
+    it('when continue button is clicked should request amend and redirect to Index', done => {
       testCustom(done, agent, underTest, [], response => {
         expect(amend.calledOnce).to.equal(true);
-        expect(response.res.headers.location).to.equal(appInstance.steps.NeedHelpWithFees.url);
+        expect(response.res.headers.location).to.equal(appInstance.steps.Index.url);
       }, 'post', true, postBody);
     });
 

e76ed56e62f2de9d61ed008e03ca228062bfed0f
Mariana <maryanasoad@hotmail.com>
2020-10-28 18:25:49 +0000

    Added Adultery, Desertion and exit scenarios

--- test/end-to-end/pages/prayer/claim-costs.js
+++ test/end-to-end/pages/prayer/claim-costs.js
@@ -1,14 +1,18 @@
 const content = require('app/steps/prayer/claim-costs/content.json').resources.en.translation.content;
 const pagePath = '/about-divorce/claim-costs';
-
 function enterClaimCosts() {
-
   const I = this;
-
   I.waitInUrl(pagePath, 5);
   I.seeCurrentUrlEquals(pagePath);
   I.checkOption(content.yes);
   I.navByClick('Continue');
 }
-
-module.exports = { enterClaimCosts };
+function enterClaimCostsCorrespondent() {
+  const I = this;
+  I.waitInUrl(pagePath, 5);
+  I.seeCurrentUrlEquals(pagePath);
+  I.checkOption(content.yes);
+  I.checkOption( '#correspondent');
+  I.navByClick('Continue');
+}
+module.exports = { enterClaimCosts, enterClaimCostsCorrespondent };

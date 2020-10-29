7c3043963b9052c43be76a8059f4925fbfb076ae
marianadpereira <71711509+marianadpereira@users.noreply.github.com>
2020-10-29 15:10:06 +0000

    Added Adultery, Desertion and exit scenarios (#764)
    Co-authored-by: Mariana <maryanasoad@hotmail.com>

---
+++ test/end-to-end/paths/adulteryAndDesertionJourneys.js
@@ -0,0 +1,124 @@
+const content = require('app/steps/grounds-for-divorce/reason/content.json').resources.en.translation.content;
+const config = require('config');
+
+Feature('Adultery and Desertion Paths E2E Tests @functional').retry(3);
+
+Before((I) => {
+  I.amOnLoadedPage('/index');
+  I.startApplication();
+  I.languagePreference();
+  I.haveBrokenMarriage();
+  I.haveRespondentAddress();
+  I.haveMarriageCert();
+  I.readFinancialRemedy();
+});
+
+Scenario('Adultery, with details', async function(I) {
+
+  I.selectHelpWithFees();
+  I.enterHelpWithFees();
+  I.selectDivorceType();
+  I.enterMarriageDate();
+
+  I.selectMarriedInUk();
+
+  I.chooseBothHabituallyResident();
+  I.chooseJurisdictionInterstitialContinue();
+
+  I.enterPeConfidentialContactDetails(false);
+  I.enterPetitionerAndRespondentNames();
+  I.enterMarriageCertificateDetails();
+  I.enterPetitionerChangedName();
+  I.enterPetitionerContactDetails();
+
+  I.enterAddressUsingPostcode('/petitioner-respondent/address');
+  I.enterCorrespondence();
+  I.selectLivingTogetherInSameProperty();
+  I.chooseRespondentServiceAddress();
+  I.enterAddressUsingPostcode('/petitioner-respondent/respondent-correspondence-address');
+
+  I.selectReasonForDivorce(content['adulteryHeading']);
+  I.selectWishToName();
+  I.enter3rdPartyDetails();
+  I.enterAddressUsingPostcode('/about-divorce/reason-for-divorce/adultery/co-respondent-address');
+  I.selectAdulteryWhere();
+  I.selectAdulteryWhen();
+  I.enterAdulteryDetails();
+  I.enterAdulterySecondHandInfo();
+
+  I.enterLegalProceedings();
+  I.selectFinancialArrangements();
+  I.enterFinancialAdvice();
+  I.enterClaimCostsCorrespondent();
+
+  if(['safari', 'microsoftEdge'].includes(config.features.browserSupport)) {
+    I.withoutUploadFile();
+  } else {
+    const isDragAndDropSupported = await I.checkElementExist('.dz-hidden-input');
+    I.uploadMarriageCertificateFile(isDragAndDropSupported);
+  }
+
+  await I.completeEquality();
+
+  I.checkMyAnswers();
+
+  const genericErrorPage = await I.checkElementExist('//h1[contains(text(), \'There has been a problem\')]');
+  if(genericErrorPage) {
+    I.checkGenericErrorPage();
+  }else {
+    I.amDoneAndSubmitted();
+  }
+}).retry(2);
+
+
+Scenario('Deserted without agreement', async function(I) {
+
+  // Fill out all of the application
+  // to test CYA content the application must be complete and valid
+  I.selectHelpWithFees(false);
+  I.selectDivorceType();
+  I.enterMarriageDate();
+
+  I.selectMarriedInUk();
+
+  I.chooseBothHabituallyResident();
+  I.chooseJurisdictionInterstitialContinue();
+
+  I.enterPeConfidentialContactDetails();
+  I.enterPetitionerAndRespondentNames();
+  I.enterMarriageCertificateDetails();
+  I.enterPetitionerChangedName();
+  I.enterPetitionerContactDetails();
+
+  I.enterAddressUsingPostcode('/petitioner-respondent/address');
+  I.enterCorrespondence();
+  I.selectLivingTogetherInSameProperty();
+
+  I.chooseRespondentServiceAddress();
+  I.enterAddressUsingPostcode('/petitioner-respondent/respondent-correspondence-address');
+  I.selectReasonForDivorce(content['desertionHeading']);
+  I.enterDesertionAgreement();
+  I.enterDesertionDate();
+  I.selectLivingApartTime();
+  I.enterDesertionDetails();
+
+  I.enterLegalProceedings();
+  I.selectFinancialArrangements();
+  I.enterFinancialAdvice();
+  I.enterClaimCosts();
+
+  const isDragAndDropSupported = await I.checkElementExist('.dz-hidden-input');
+  I.uploadMarriageCertificateFile(isDragAndDropSupported);
+
+  await I.completeEquality();
+
+  I.checkDesertionDateOnCYAPage();
+  I.checkMyAnswers();
+
+  const genericErrorPage = await I.checkElementExist('//h1[contains(text(), \'There has been a problem\')]');
+  if(genericErrorPage) {
+    I.checkGenericErrorPage();
+  }else {
+    I.confirmIWillPayOnline();
+  }
+}).retry(2);

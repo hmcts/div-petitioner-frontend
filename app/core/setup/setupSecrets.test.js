47d918e851b29cfe80abe45e9311dd7d6a64a5d7
Timothy Ajisafe <timothy.ajisafe@hmcts.net>
2020-11-06 11:53:03 +0000

    Timothy Ajisafe - RPET-586 smoke test update

--- app/core/setup/setupSecrets.test.js
+++ app/core/setup/setupSecrets.test.js
@@ -1,5 +1,5 @@
-const { expect } = require('test/util/chai');
-const { cloneDeep } = require('lodash');
+const {expect} = require('test/util/chai');
+const {cloneDeep} = require('lodash');
 const config = require('config');
 const proxyquire = require('proxyquire');
 
@@ -28,7 +28,7 @@ describe(modulePath, () => {
 
       // Update config with secret setup
       const setupSecrets = proxyquire(modulePath,
-        { config: mockConfig });
+        {config: mockConfig});
       setupSecrets.setup();
 
       expect(mockConfig.secret)
@@ -41,12 +41,16 @@ describe(modulePath, () => {
         .to.equal(mockConfig.secrets.div['os-places-token']);
       expect(mockConfig.services.serviceAuthProvider.microserviceKey)
         .to.equal(mockConfig.secrets.div['frontend-secret']);
+      expect(mockConfig.testUser.idamCitizenEmail)
+        .to.equal(mockConfig.secrets.div['idam-citizen-username']);
+      expect(mockConfig.testUser.idamCitizenPassword)
+        .to.equal(mockConfig.secrets.div['idam-citizen-password']);
     });
 
     it('should not set config values when secrets path is not set', () => {
       // Update config with secret setup
       const setupSecrets = proxyquire(modulePath,
-        { config: mockConfig });
+        {config: mockConfig});
       setupSecrets.setup();
 
       expect(mockConfig.secret)
@@ -59,14 +63,18 @@ describe(modulePath, () => {
         .to.equal(config.services.postcodeInfo.token);
       expect(mockConfig.services.serviceAuthProvider.microserviceKey)
         .to.equal(config.services.serviceAuthProvider.microserviceKey);
+      expect(mockConfig.testUser.idamCitizenEmail)
+        .to.equal(config.testUser.idamCitizenEmail);
+      expect(mockConfig.testUser.idamCitizenPassword)
+        .to.equal(config.testUser.idamCitizenPassword);
     });
 
     it('should only set one config value when single secret path is set', () => {
-      mockConfig.secrets = { div: { 'idam-secret': 'idamValue' } };
+      mockConfig.secrets = {div: {'idam-secret': 'idamValue'}};
 
       // Update config with secret setup
       const setupSecrets = proxyquire(modulePath,
-        { config: mockConfig });
+        {config: mockConfig});
       setupSecrets.setup();
 
       expect(mockConfig.secret)
@@ -81,6 +89,10 @@ describe(modulePath, () => {
         .to.equal(config.services.postcodeInfo.token);
       expect(mockConfig.services.serviceAuthProvider.microserviceKey)
         .to.equal(config.services.serviceAuthProvider.microserviceKey);
+      expect(mockConfig.testUser.idamCitizenEmail)
+        .to.equal(config.testUser.idamCitizenEmail);
+      expect(mockConfig.testUser.idamCitizenPassword)
+        .to.equal(config.testUser.idamCitizenPassword);
     });
   });
 });

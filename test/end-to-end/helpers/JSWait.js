e1efbf4c1ef434440a4f1a0c02e28347f0211b60
Rob Doherty-Kelly <Rob-Doherty@users.noreply.github.com>
2020-11-27 11:38:33 +0000

    Nightly Crossbrowser improvements (#786)
    * chore: rename of e2e retry functionality in idamHelper.js to be clearer what's happening
    
    * refactor: await webdriver.browser.setWindowSize() to wait for IE11 browser resize before continuing with crossbrowser tests
    
    * refactor: various changes to support running the crossbrowser tests in the nightly test pipeline
    
    * refactor: various changes to support running the crossbrowser tests in the nightly test pipeline

--- test/end-to-end/helpers/JSWait.js
+++ test/end-to-end/helpers/JSWait.js
@@ -1,24 +1,24 @@
 class JSWait extends codecept_helper {
 
   _beforeStep(step) {
-
     const helper = this.helpers['WebDriver'] || this.helpers['Puppeteer'];
 
     // Wait for content to load before checking URL
     if (step.name === 'seeCurrentUrlEquals' || step.name === 'seeInCurrentUrl') {
       return helper.wait(2);
     }
+    return step;
   };
 
   async navByClick (text, locator) {
     const helper = this.helpers['WebDriver'] || this.helpers['Puppeteer'];
     const helperIsPuppeteer = this.helpers['Puppeteer'];
 
-    helper.click(text, locator).catch(err => { console.error(err.message); });
-
     if (helperIsPuppeteer) {
+      helper.click(text, locator).catch(err => { console.error(err.message); });
       await helper.page.waitForNavigation({waitUntil: 'networkidle0'});
     } else {
+      await helper.click(text, locator).catch(err => { console.error(err.message); });
       await helper.wait(2);
     }
   }
--- test/end-to-end/pages/help/with-fees.js
+++ test/end-to-end/pages/help/with-fees.js
@@ -1,4 +1,4 @@
-const contentEn = require('app/steps/help/need-help/content.json').resources.en.translation.content;
+const contentEn = require('app/steps/help/with-fees/content.json').resources.en.translation.content;
 const contentCy = require('app/steps/help/with-fees/content.json').resources.cy.translation.content;
 const commonContentEn = require('app/content/common-en').resources.en.translation;
 const commonContentCy = require('app/content/common-cy').resources.cy.translation;

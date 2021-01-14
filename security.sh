31a5ccb9d0967f45e94bdd7b8ba3dfb5724fc9de
Patryk Jar <jar.patryk@gmail.com>
2021-01-14 18:14:22 +0100

    RPET-653: zap report

--- security.sh
+++ security.sh
@@ -21,7 +21,7 @@ while !(curl -s http://0.0.0.0:1001) > /dev/null
   cp *.html functional-output/
   cp activescanReport.xml functional-output/
 
-  zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Low --exit-code False
+  zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l High --exit-code False
   curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
   cp *.* functional-output/
 

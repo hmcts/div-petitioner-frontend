9779b0af36f70a6a485086d2fd734bf177ff3b84
Felix Eyetan <eyetanfelix@gmail.com>
2021-03-02 17:49:27 +0000

    Adding new repondent journey toggle in config

--- app/core/utils/checkToggle.js
+++ app/core/utils/checkToggle.js
@@ -1,10 +1,16 @@
 const { get } = require('lodash');
 
-const isToggleOnAwaitingAmend = session => {
+const isToggleOnAwaitingAmend = (session) => {
   const toggle = get(session, 'featureToggles', { ft_awaiting_amend: false });
   return toggle.ft_awaiting_amend;
 };
 
+const isToggleOnRepresentedRespondentJourney = (session) => {
+  const toggle = get(session, 'featureToggles', { ft_represented_respondent_journey: false });
+  return toggle.ft_represented_respondent_journey;
+};
+
 module.exports = {
-  isToggleOnAwaitingAmend
+  isToggleOnAwaitingAmend,
+  isToggleOnRepresentedRespondentJourney
 };
--- app/core/utils/checkToggle.test.js
+++ app/core/utils/checkToggle.test.js
@@ -1,24 +1,34 @@
-const { isToggleOnAwaitingAmend } = require('app/core/utils/checkToggle');
+const forEach = require('mocha-each');
+const {invoke} = require('lodash');
+const {expect} = require('test/util/chai');
 
-const { expect } = require('test/util/chai');
+const checkToggle = require('app/core/utils/checkToggle');
+
+const buildSessionWithFeatureToggle = (featureName, featureValue) => {
+  const featureToggles = {};
+  featureToggles[featureName] = featureValue;
+  return {featureToggles};
+};
 
 describe('checkToggle', () => {
-  it('should return true when toggle is on', () => {
-    const session = {
-      featureToggles: { ft_awaiting_amend: true }
-    };
-    expect(isToggleOnAwaitingAmend(session)).to.equal(true);
-  });
 
-  it('should return false when toggle is off', () => {
-    const session = {
-      featureToggles: { ft_awaiting_amend: false }
-    };
-    expect(isToggleOnAwaitingAmend(session)).to.equal(false);
-  });
+  forEach([
+    ['ft_awaiting_amend', 'isToggleOnAwaitingAmend'],
+    ['ft_represented_respondent_journey', 'isToggleOnRepresentedRespondentJourney']
+  ])
+    .describe('%s', (featureToggleName, featureToggleMethod) => {
+      it(`should return true when ${featureToggleName} toggle is on`, () => {
+        const session = buildSessionWithFeatureToggle(featureToggleName, true);
+        expect(invoke(checkToggle, featureToggleMethod, session)).to.equal(true);
+      });
 
-  it('should return false when toggle does not exist in session', () => {
-    const session = {};
-    expect(isToggleOnAwaitingAmend(session)).to.equal(false);
-  });
+      it(`should return false when ${featureToggleName} toggle is off`, () => {
+        const session = buildSessionWithFeatureToggle(featureToggleName, false);
+        expect(invoke(checkToggle, featureToggleMethod, session)).to.equal(false);
+      });
+
+      it(`should return false when ${featureToggleName} toggle does not exist in session`, () => {
+        expect(invoke(checkToggle, featureToggleMethod, {})).to.equal(false);
+      });
+    });
 });

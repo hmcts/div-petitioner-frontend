ff0a2db543fbee4d7e403fb3a3bfb8e3e42eb820
James Nelson <Jamesnelson117@gmail.com>
2021-03-03 10:55:41 +0000

    DIV-6816: Added represented_respondent_journey LD toggle

--- app/routes/featureToggles.js
+++ app/routes/featureToggles.js
@@ -10,12 +10,20 @@ const fetchAwaitingAmendToggleVariation = (req, res, next) => {
   return featureToggle.callCheckToggle(req, res, next, res.locals.launchDarkly, 'ft_awaiting_amend', featureToggle.toggleFeature);
 };
 
+const fetchRepresentedRespondentJourneyToggleVariation = (req, res, next) => {
+  return featureToggle.callCheckToggle(req, res, next, res.locals.launchDarkly, 'ft_represented_respondent_journey', featureToggle.toggleFeature);
+};
+
 router.use([fetchAwaitingAmendToggleVariation], (req, res, next) => {
   next();
 });
 
 router.get('*', (req, res, next) => {
   return featureToggle.callCheckToggle(req, res, next, res.locals.launchDarkly, 'ft_welsh', featureToggle.toggleFeature);
+});
+
+router.use([fetchRepresentedRespondentJourneyToggleVariation], (req, res, next) => {
+  next();
 });
 
 router.get('/equality-and-diversity', (req, res, next) => {
--- test/end-to-end/data/basicDivorceSessionData.js
+++ test/end-to-end/data/basicDivorceSessionData.js
@@ -358,6 +358,7 @@ module.exports = {
       postcode: 'SW9 9PE'
     },
     featureToggles: {
+      ft_represented_respondent_journey: 'false',
       ft_awaiting_amend: 'true',
       ft_welsh: 'true'
     },
@@ -667,6 +668,7 @@ module.exports = {
       postcode: 'SW9 9PE'
     },
     featureToggles: {
+      ft_represented_respondent_journey: 'false',
       ft_awaiting_amend: 'true',
       ft_welsh: 'true'
     },
@@ -1285,6 +1287,7 @@ module.exports = {
       postcode: 'SW9 9PE'
     },
     featureToggles: {
+      ft_represented_respondent_journey: 'false',
       ft_awaiting_amend: 'true',
       ft_welsh: 'true'
     },
@@ -1317,6 +1320,7 @@ module.exports = {
   reasonForDivorceClaimingAdultery: false,
   marriageCertificateFiles: [],
   featureToggles: {
+    ft_represented_respondent_journey: 'false',
     ft_awaiting_amend: false,
     ft_welsh: true
   }

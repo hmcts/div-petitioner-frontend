2dc3664cd23d059f7742ba6c5659cf890ee2b31a
selvameenad <selvameena.cognizant@gmail.com>
2021-03-04 17:13:13 +0000

    INtegrating Antenna changes

--- app.js
+++ app.js
@@ -61,13 +61,23 @@ exports.init = listenForConnections => {
         'www.google-analytics.com',
         'hmctspiwik.useconnect.co.uk',
         'vcc-eu4.8x8.com',
-        'vcc-eu4b.8x8.com'
+        'vcc-eu4b.8x8.com',
+        'https://webchat-client.ctsc.hmcts.net'
       ],
-      connectSrc: ['\'self\''],
-      mediaSrc: ['\'self\''],
+      connectSrc: [
+        '\'self\'',
+        'https://webchat-client.ctsc.hmcts.net',
+        'https://webchat.ctsc.hmcts.net',
+        'wss://webchat.ctsc.hmcts.net'
+      ],
+      mediaSrc: [
+        '\'self\'',
+        'https://webchat-client.ctsc.hmcts.net'
+      ],
       frameSrc: [
         'vcc-eu4.8x8.com',
-        'vcc-eu4b.8x8.com'
+        'vcc-eu4b.8x8.com',
+        'https://webchat-client.ctsc.hmcts.net/chat-client/1/'
       ],
       imgSrc: [
         '\'self\'',
@@ -117,7 +127,7 @@ exports.init = listenForConnections => {
     loader: nunjucks.FileSystemLoader,
     globals: {
       webchat: CONF.services.webchat,
-      features: { webchat: parseBool(CONF.features.webchat) }
+      features: { webchat: parseBool(CONF.features.webchat), antennaWebchat: parseBool(CONF.features.antennaWebchat) }
     }
   });
 
@@ -137,6 +147,8 @@ exports.init = listenForConnections => {
   // Middleware to serve static assets
   app.use('/public', express.static(`${__dirname}/public`));
   app.use('/webchat', express.static(`${__dirname}/node_modules/@hmcts/ctsc-web-chat/assets`));
+  app.use('/public/locale', express.static(`${__dirname}/app/assets/locale`));
+  app.use('/public/javascripts', express.static(`${__dirname}/app/assets/javascripts`));
 
   // Parsing cookies for the stored encrypted session key
   app.use(cookieParser());

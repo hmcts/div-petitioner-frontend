6e32bebc5700e0f9102f6f65d4700ff289cc62f8
James Nelson <Jamesnelson117@gmail.com>
2021-03-05 14:58:12 +0000

    DIV-6760: Added organisationService

---
+++ app/services/mocks/organisationServiceClient.js
@@ -0,0 +1,40 @@
+const organisations = [
+  {
+    contactInformation: [
+      {
+        addressLine1: '1 Trasna way',
+        addressLine2: 'Lurgan',
+        addressLine3: '',
+        country: 'United Kingdom',
+        county: 'Armagh',
+        postCode: 'BT25 545',
+        townCity: 'Craigavon'
+      }
+    ],
+    name: 'Ernser Inc',
+    organisationIdentifier: '21-3701590'
+  },
+  {
+    contactInformation: [
+      {
+        addressLine1: '10 Lakeview',
+        addressLine2: 'Crumlin',
+        addressLine3: '',
+        country: 'United Kingdom',
+        county: 'Down',
+        postCode: 'BT21 525',
+        townCity: 'Ballymena'
+      }
+    ],
+    name: 'Zboncak and Sons',
+    organisationIdentifier: '25-3701590'
+  }
+];
+
+module.exports = {
+  getOrganisationByName: () => {
+    return new Promise(resolve => {
+      resolve(organisations);
+    });
+  }
+};

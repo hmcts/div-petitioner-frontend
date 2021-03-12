87d3893255637e2bf6d8572e651a948513966de4
jackMur <jackmur@kainos.com>
2021-03-12 17:31:39 +0000

    Fix a11y tests

---
+++ test/end-to-end/paths/IssuedToBailiff.js
@@ -0,0 +1,15 @@
+const content = require('app/steps/issued-to-bailiff/content.json').resources.en.translation.content;
+const whoResponds = content.yourResponds.replace('{{ divorceWho }}', 'wife');
+
+Feature('IssuedToBailiff - Sent to Bailiff path @functional').retry(2);
+
+Scenario.only('Issued-to-bailiff page is displayed when IssuedToBailiff case', async function (I) {
+  I.startApplicationWith('IssuedToBailiff');
+  I.amOnLoadedPage('/issued-to-bailiff');
+
+  I.see(content.mainHeading);
+  I.see(whoResponds);
+  I.see('Your application for ‘personal service by a court bailiff’');
+
+  // TODO: Ensure this works. This was made prior to development code being added.
+});

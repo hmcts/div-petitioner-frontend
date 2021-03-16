b855f3ff85d842522a9320922ed6bfe6aa61c694
James Nelson <Jamesnelson117@gmail.com>
2021-03-16 10:38:10 +0000

    DIV-6760: Mock organisations updated to match AAT results

--- test/end-to-end/pages/organisation-search/search.js
+++ test/end-to-end/pages/organisation-search/search.js
@@ -2,7 +2,8 @@ const commonContentEn = require('app/content/common-en').resources.en.translatio
 const commonContentCy = require('app/content/common-cy').resources.cy.translation;
 
 const mockOrganisation = {
-  reference: '02-002',
+  reference: '1YSAUCP',
+  searchTerm: 'Divorce',
   name: 'Karen Fox',
   email: 'karen@karenfox.co.uk'
 };
@@ -16,9 +17,9 @@ function enterOrganisationUsingLookup(language = 'en', stepUrl, organisation = m
   I.waitInUrl(stepUrl);
   I.seeInCurrentUrl(stepUrl);
   I.waitForVisible('#solicitor-search');
-  I.fillField('respondentSolicitorFirm', organisation.name);
+  I.fillField('respondentSolicitorFirm', organisation.searchTerm);
   I.navByClick(findAddress);
-  I.wait(4);
+  I.wait(10);
   I.waitForVisible('#solicitor-search-results');
   I.navByClick(`select-${organisation.reference}`);
   I.wait(4);

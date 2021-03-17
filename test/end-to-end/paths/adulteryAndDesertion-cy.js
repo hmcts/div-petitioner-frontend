2b16d2da7f7bea2e73fb4594cc576faa50ca57b8
Patryk Jar <jar.patryk@gmail.com>
2021-03-17 14:05:54 +0100

    fix

--- test/end-to-end/paths/adulteryAndDesertion-cy.js
+++ test/end-to-end/paths/adulteryAndDesertion-cy.js
@@ -70,7 +70,6 @@ Scenario('Adultery, with details (cy)', async function(I) {
 
 Scenario(`${language.toUpperCase()} - Desertion without agreement (cy)`, async function(I) {
 
-  const divorceReason = contentCy;
   await stepsStartApplicationToReadFinancialRemedy(I, language);
 
   I.selectHelpWithFees(language, false);

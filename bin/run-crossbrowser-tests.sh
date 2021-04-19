de78aa6caf8b2cf438cf752a01fe20f38c6502df
J. Victor Ferreira <victor.ferreira@hmcts.net>
2021-04-19 15:54:07 +0100

    DIV-6907: Enabling IDAM feature for nightly pipeline (#864)

--- bin/run-crossbrowser-tests.sh
+++ bin/run-crossbrowser-tests.sh
@@ -3,7 +3,6 @@ set -ex
 
 # Setup required for Saucelabs environment variables. TEST_URL should be set by CNP
 export E2E_FRONTEND_URL=${TEST_URL}
-export FEATURE_IDAM=true
 export IDAM_API_URL=${IDAM_API_URL:-"https://idam-api.aat.platform.hmcts.net"}
 export IGNORE_SESSION_VALIDATION=true
 

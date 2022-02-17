3db2488cb1a65a3d0255d3bcbbeab6e84018ddaa
Barry Weeks <barry.weeks@justice.gov.uk>
2022-02-17 10:14:06 +0000

    Log cookie domain
    Log cookie domain for debug purposes

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ if (CONF.environment !== 'testing') {
 
 const listenForConnections = true;
 
+const logger = require('@hmcts/nodejs-logging').Logger.getLogger(__filename);
+logger.info(`
+
+              ==========================================================================================
+                Cookie Domain: ${CONF.cookieDomain}
+              ==========================================================================================
+`);
+
 if (CONF.applicationInsights.instrumentationKey) {
   appInsights.setup(CONF.applicationInsights.instrumentationKey)
     .setAutoCollectConsole(true, true)

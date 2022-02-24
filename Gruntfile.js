40cfcb6684668256a0659d2baae896134c8230e4
Barry Weeks <barry.weeks@justice.gov.uk>
2022-02-24 16:49:04 +0000

    Revert "Move webpack cookie domain init to gruntfile"
    This reverts commit 71ead6d0db740803fb4987bd7e7fd468229d2703.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
 const ArchivePlugin = require('webpack-archive-plugin');
-const CONF = require("config");
 
 
 module.exports = function(grunt) {
@@ -18,9 +17,7 @@ module.exports = function(grunt) {
       prod: {
         plugins: webpackConfig.plugins.concat(
           new webpack.DefinePlugin({ 'process.env': { NODE_ENV: 'production' } }),
-          new webpack.optimize.UglifyJsPlugin(),
-          // Replace variable values of COOKIEDOMAIN in JS files with the value of CONF.cookieDomain (as a quoted str)
-          new webpack.DefinePlugin({'COOKIEDOMAIN': JSON.stringify(CONF.cookieDomain)}),
+          new webpack.optimize.UglifyJsPlugin()
         )
       }
     },

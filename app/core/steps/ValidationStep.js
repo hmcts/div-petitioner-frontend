f2d29316dd481e138c82c46f6c90049ed6ceafa2
Barry Weeks <barry.weeks@justice.gov.uk>
2022-03-01 16:33:08 +0000

    Update ValidationStep.js

--- app/core/steps/ValidationStep.js
+++ app/core/steps/ValidationStep.js
@@ -21,6 +21,7 @@ const removeEmptyValues = require('app/core/helpers/removeEmptyValues');
 const stepsHelper = require('app/core/helpers/steps');
 
 const ajv = new Ajv({ allErrors: true, v5: true });
+ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
 
 module.exports = class ValidationStep extends Step {
   get middleware() {

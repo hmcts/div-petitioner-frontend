e23829a8298c45c2523fc759580adc826e172739
Barry Weeks <barry.weeks@justice.gov.uk>
2022-03-03 16:47:10 +0000

    Update dependencies
    Update dependencies and apply necessary code changes.

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
     css: './tmp/sass/application.scss'
   },
   output: {
-    path: './public/[hash]',
+    path: path.resolve(__dirname, './public/[hash]'),
     filename: 'javascripts/bundle--[name].js'
   },
   plugins: [
@@ -59,6 +59,9 @@ module.exports = {
       });
     }
   ],
+  optimization: {
+    minimize: true
+  },
   module: {
     rules: [
       {
@@ -91,9 +94,7 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
-      }
-    ],
-    loaders: [
+      },
       { test: /public/, loader: 'imports-loader?this=>window' },
       { test: /public/, loader: 'imports-loader?$=jquery' }
     ]

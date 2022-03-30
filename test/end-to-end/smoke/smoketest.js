9d2e5969d7a9eb4f56d7507c6ae532a30fd06fb1
Barry Weeks <barry.weeks@justice.gov.uk>
2022-03-30 17:55:39 +0100

    Update smoketest.js
    Test disabling most actions

--- test/end-to-end/smoke/smoketest.js
+++ test/end-to-end/smoke/smoketest.js
@@ -1,10 +1,10 @@
 Feature('Smoke test', {retries: 2});
 
 Scenario('Smoke Test ', (I) => {
-  I.amOnLoadedPage('/');
+  // I.amOnLoadedPage('/');
   // I.startApplication();
   I.wait(1);
   // I.languagePreference();
-  I.signOut();
+  // I.signOut();
 });
 

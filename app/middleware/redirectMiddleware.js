2b996adfb7d5e88590667cb345c07b3d6b242ced
Ruban <ruban72@hotmail.com>
2022-03-31 22:53:31 +0100

    DFR-981: Remove feature flag

--- app/middleware/redirectMiddleware.js
+++ app/middleware/redirectMiddleware.js
@@ -1,31 +1,32 @@
 const CONF = require('config');
 const _ = require('lodash');
 const logger = require('app/services/logger').logger(__filename);
+
 const authTokenString = '__auth-token';
 
 const redirectOnCondition = (req, res, next) => {
-    const session = req.session;
-    const caseState = _.get(session, 'state');
-    const courtId = _.get(session, 'allocatedCourt.courtId', _.get(session, 'courts'));
-    const caseId = _.get(session, 'caseId');
-    logger.infoWithReq(req, caseId);
-    const redirectionStates = CONF.newAppCutoffRedirectStates;
-    logger.infoWithReq(req, `Redirection States: ${redirectionStates}`);
-    const redirect = redirectionStates.includes(caseState) || !caseState;
-    logger.infoWithReq(req, `Redirect: ${redirect}`);
+  const session = req.session;
+  const caseState = _.get(session, 'state');
+  const courtId = _.get(session, 'allocatedCourt.courtId', _.get(session, 'courts'));
+  const caseId = _.get(session, 'caseId');
+  logger.infoWithReq(req, caseId);
+  const redirectionStates = CONF.newAppCutoffRedirectStates;
+  logger.infoWithReq(req, `Redirection States: ${redirectionStates}`);
+  const redirect = redirectionStates.includes(caseState) || !caseState;
+  logger.infoWithReq(req, `Redirect: ${redirect}`);
 
-    logger.infoWithReq(req, 'PFE redirect check', `Case Ref: ${caseId}. Case State: ${caseState}. Court ID: ${courtId}.`);
-    if (caseState && CONF.ccd.courts.includes(courtId) && !CONF.ccd.d8States.includes(caseState)) {
-        logger.infoWithReq(req, 'PFE redirecting to DN', `Case Ref: ${caseId}. Redirect check Passed.`);
-        const appLandingPage = `${CONF.apps.dn.url}${CONF.apps.dn.landing}`;
-        const queryString = `?${authTokenString}=${req.cookies[authTokenString]}`;
-        return res.redirect(`${appLandingPage}${queryString}`);
-        // eslint-disable-next-line no-undefined
-    } else if (!typeof caseId === undefined || redirect) {
-        return res.redirect('/cutoff-landing-page');
-    }
+  logger.infoWithReq(req, 'PFE redirect check', `Case Ref: ${caseId}. Case State: ${caseState}. Court ID: ${courtId}.`);
+  if (caseState && CONF.ccd.courts.includes(courtId) && !CONF.ccd.d8States.includes(caseState)) {
+    logger.infoWithReq(req, 'PFE redirecting to DN', `Case Ref: ${caseId}. Redirect check Passed.`);
+    const appLandingPage = `${CONF.apps.dn.url}${CONF.apps.dn.landing}`;
+    const queryString = `?${authTokenString}=${req.cookies[authTokenString]}`;
+    return res.redirect(`${appLandingPage}${queryString}`);
+    // eslint-disable-next-line no-undefined
+  } else if (!typeof caseId === undefined || redirect) {
+    return res.redirect('/cutoff-landing-page');
+  }
 
-    return next();
+  return next();
 };
 
-module.exports = {redirectOnCondition};
+module.exports = { redirectOnCondition };

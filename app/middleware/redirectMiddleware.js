e6c07668c52ac4b68707cf30b4c0b4d44e309bca
Barry Weeks <barry.weeks@justice.gov.uk>
2022-03-31 23:27:26 +0100

    Update redirectMiddleware.js

--- app/middleware/redirectMiddleware.js
+++ app/middleware/redirectMiddleware.js
@@ -16,6 +16,9 @@ const redirectOnCondition = (req, res, next) => {
   logger.infoWithReq(req, `Redirection States: ${redirectionStates}`);
   const redirect = redirectionStates.includes(caseState) || !caseState;
   logger.infoWithReq(req, `Redirect: ${redirect}`);
+  logger.infoWithReq(req, `Toggle: ${CONF.features.newAppCutoff}`);
+  // eslint-disable-next-line no-undefined
+  logger.infoWithReq(req, `TypeOf CaseId: ${typeof caseId === undefined}`);
 
   logger.infoWithReq(req, 'PFE redirect check', `Case Ref: ${caseId}. Case State: ${caseState}. Court ID: ${courtId}.`);
   if (caseState && CONF.ccd.courts.includes(courtId) && !CONF.ccd.d8States.includes(caseState)) {
@@ -23,15 +26,6 @@ const redirectOnCondition = (req, res, next) => {
     const appLandingPage = `${CONF.apps.dn.url}${CONF.apps.dn.landing}`;
     const queryString = `?${authTokenString}=${req.cookies[authTokenString]}`;
     return res.redirect(`${appLandingPage}${queryString}`);
-  }
-
-  logger.infoWithReq(req, `Toggle: ${CONF.features.newAppCutoff}`);
-  // eslint-disable-next-line no-undefined
-  logger.infoWithReq(req, `TypeOf CaseId: ${typeof caseId === undefined}`);
-  // eslint-disable-next-line no-undefined
-  if (JSON.parse(CONF.features.newAppCutoff) && (!typeof caseId === undefined || redirect)) {
-    logger.infoWithReq(req, 'PFE redirecting to Landing Page', `Case Ref: ${caseId}. Redirect check Passed.`);
-    return res.redirect('/cutoff-landing-page');
   }
 
   return next();

0dea27b715178036ed8962f7b00f04209acf3a55
Barry Weeks <barry.weeks@justice.gov.uk>
2022-03-31 21:48:26 +0100

    Update redirectMiddleware.js

--- app/middleware/redirectMiddleware.js
+++ app/middleware/redirectMiddleware.js
@@ -4,42 +4,21 @@ const logger = require('app/services/logger').logger(__filename);
 
 const authTokenString = '__auth-token';
 
-/* eslint-disable complexity */
 const redirectOnCondition = (req, res, next) => {
   const session = req.session;
-  logger.infoWithReq(req, JSON.stringify(session));
   const caseState = _.get(session, 'state');
-  logger.infoWithReq(req, caseState);
   const courtId = _.get(session, 'allocatedCourt.courtId', _.get(session, 'courts'));
-  logger.infoWithReq(req, courtId);
   const caseId = _.get(session, 'caseId');
-  logger.infoWithReq(req, caseId);
-  const redirectionStates = CONF.newAppCutoffRedirectStates;
-  logger.infoWithReq(req, redirectionStates);
-  const redirect = redirectionStates.includes(caseState) || !caseState;
-  logger.infoWithReq(req, redirect);
 
   logger.infoWithReq(req, 'PFE redirect check', `Case Ref: ${caseId}. Case State: ${caseState}. Court ID: ${courtId}.`);
-  logger.infoWithReq(req, JSON.stringify(session));
-  logger.infoWithReq(req, `
-    =================================================================================================================
-      Case Id: ${caseId}
-      Case State: ${caseState}
-      State Redirect: ${redirect}
-    =================================================================================================================
-  `);
   if (caseState && CONF.ccd.courts.includes(courtId) && !CONF.ccd.d8States.includes(caseState)) {
     logger.infoWithReq(req, 'PFE redirecting to DN', `Case Ref: ${caseId}. Redirect check Passed.`);
     const appLandingPage = `${CONF.apps.dn.url}${CONF.apps.dn.landing}`;
     const queryString = `?${authTokenString}=${req.cookies[authTokenString]}`;
     return res.redirect(`${appLandingPage}${queryString}`);
-    // eslint-disable-next-line no-undefined
-  } else if (JSON.parse(CONF.features.newAppCutoff) && (!typeof caseId === undefined || redirect)) {
-    return res.redirect('/cutoff-landing-page');
   }
 
   return next();
 };
-/* eslint-enable complexity */
 
 module.exports = { redirectOnCondition };

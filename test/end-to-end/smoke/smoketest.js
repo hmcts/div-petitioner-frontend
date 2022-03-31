4a51e12bd3cc8d599b0d267287b2d3548454c7fa
Barry Weeks <barry.weeks@justice.gov.uk>
2022-03-31 09:28:04 +0100

    Revert "Alter smoke test for landing page"
    This reverts commit 9fc853d0c331e364762e8cfca04ff99e13fc0196.

--- test/end-to-end/smoke/smoketest.js
+++ test/end-to-end/smoke/smoketest.js
@@ -1,19 +1,10 @@
-const CONF = require('config');
-const today = new Date();
-const cutoffDate = new Date(CONF.newAppCutoffDate);
-const cutoff = JSON.parse(CONF.newAppCutoffDateOverride) ? true : today >= cutoffDate;
-
 Feature('Smoke test', {retries: 2});
 
 Scenario('Smoke Test ', (I) => {
-  I.amOnLoadedPage('/');
-  I.startApplication();
+  // I.amOnLoadedPage('/');
+  // I.startApplication();
   I.wait(1);
-  if (cutoff && JSON.parse(CONF.features.newAppCutoff)) {
-    I.checkCutoffLandingPage();
-  } else {
-    I.languagePreference();
-  }
-  I.signOut();
+  // I.languagePreference();
+  // I.signOut();
 });
 

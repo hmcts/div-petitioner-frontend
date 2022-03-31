0be898f5351b711af541c23aa889147a42c3f57d
Ruban <ruban72@hotmail.com>
2022-04-01 00:56:24 +0100

    DFR-981: add new middleware for redirect to landing page

--- app/middleware/redirectMiddleware.js
+++ app/middleware/redirectMiddleware.js
@@ -1,7 +1,6 @@
 const CONF = require('config');
 const _ = require('lodash');
 const logger = require('app/services/logger').logger(__filename);
-const parseBool = require('app/core/utils/parseBool');
 
 const authTokenString = '__auth-token';
 
@@ -10,22 +9,15 @@ const redirectOnCondition = (req, res, next) => {
   const caseState = _.get(session, 'state');
   const courtId = _.get(session, 'allocatedCourt.courtId', _.get(session, 'courts'));
   const caseId = _.get(session, 'caseId');
-  const redirectFeatureOn = parseBool(CONF.features.newAppCutoff);
-  logger.infoWithReq(req, caseId);
-  const redirectionStates = CONF.newAppCutoffRedirectStates;
-  logger.infoWithReq(req, `Redirection States: ${redirectionStates}`);
-  const redirect = redirectionStates.includes(caseState) || !caseState;
-  logger.infoWithReq(req, `Redirect: ${redirect}`);
-  logger.infoWithReq(req, `Redirect feature: ${redirectFeatureOn}`);
+
   logger.infoWithReq(req, 'PFE redirect check', `Case Ref: ${caseId}. Case State: ${caseState}. Court ID: ${courtId}.`);
   if (caseState && CONF.ccd.courts.includes(courtId) && !CONF.ccd.d8States.includes(caseState)) {
     logger.infoWithReq(req, 'PFE redirecting to DN', `Case Ref: ${caseId}. Redirect check Passed.`);
     const appLandingPage = `${CONF.apps.dn.url}${CONF.apps.dn.landing}`;
     const queryString = `?${authTokenString}=${req.cookies[authTokenString]}`;
     return res.redirect(`${appLandingPage}${queryString}`);
-  } else if (redirectFeatureOn) {
-    return res.redirect('/cutoff-landing-page');
   }
+
   return next();
 };
 

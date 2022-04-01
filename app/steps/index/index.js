13925884eca02935e9541d85758c4694b623d64d
Barry Weeks <barry.weeks@justice.gov.uk>
2022-04-01 13:15:29 +0100

    Update index.js
    Add output to log

--- app/steps/index/index.js
+++ app/steps/index/index.js
@@ -1,6 +1,7 @@
 const Step = require('app/core/steps/Step');
 const { authenticate } = require('app/services/idam');
 const initSession = require('app/middleware/initSession');
+const logger = require('app/services/logger').logger(__filename);
 
 module.exports = class Index extends Step {
   get url() {
@@ -12,11 +13,14 @@ module.exports = class Index extends Step {
   }
 
   next(ctx, session) {
+    logger.infoWithReq(null, `Index Next Step: ${session} `);
     return this.nextStep(session);
   }
 
   get middleware() {
+    logger.infoWithReq(null, 'Index get middleware');
     const idamAuthenticate = (req, res, next) => {
+      logger.infoWithReq(req, 'idamAuthenticate');
       const auth = authenticate('https', req.get('host'), '/authenticated', req.session.language);
       return auth(req, res, next);
     };

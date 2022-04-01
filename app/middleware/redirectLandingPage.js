0b2a9acbce72933aa65994a5251c397520118b5b
Ruban <ruban72@hotmail.com>
2022-04-01 01:15:17 +0100

    DFR-981: add new middleware for redirect to landing page

--- app/middleware/redirectLandingPage.js
+++ app/middleware/redirectLandingPage.js
@@ -5,17 +5,11 @@ const parseBool = require('app/core/utils/parseBool');
 
 const redirectToLandingPageOnCondition = (req, res, next) => {
   const session = req.session;
-  const caseState = _.get(session, 'state');
   const caseId = _.get(session, 'caseId');
   const redirectFeatureOn = parseBool(CONF.features.newAppCutoff);
   logger.infoWithReq(req, `Redirect feature: ${redirectFeatureOn}`);
   logger.infoWithReq(req, `Case Ref: ${caseId}`);
-  const redirectionStates = CONF.newAppCutoffRedirectStates;
-  logger.infoWithReq(req, `Redirection States: ${redirectionStates}`);
-  const redirect = redirectionStates.includes(caseState) || !caseState;
-  logger.infoWithReq(req, `Redirect: ${redirect}`);
-  logger.infoWithReq(req, 'PFE redirect landing page check', `Case Ref: ${caseId}. Case State: ${caseState}`);
-  if (redirectFeatureOn && redirect) {
+  if (redirectFeatureOn) {
     return res.redirect('/cutoff-landing-page');
   }
   return next();

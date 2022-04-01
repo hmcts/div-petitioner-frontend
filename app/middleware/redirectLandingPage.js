b93a1575020775f749da660a977f5411001ad00d
Ruban <ruban72@hotmail.com>
2022-04-01 07:34:25 +0100

    DFR-981: Test redirect

--- app/middleware/redirectLandingPage.js
+++ app/middleware/redirectLandingPage.js
@@ -2,6 +2,8 @@ const CONF = require('config');
 const logger = require('app/services/logger').logger(__filename);
 const parseBool = require('app/core/utils/parseBool');
 
+const authTokenString = '__auth-token';
+
 const redirectToLandingPageOnCondition = (req, res, next) => {
   const redirectFeatureOn = parseBool(CONF.features.newAppCutoff);
   const redirectionStates = CONF.newAppCutoffRedirectStates;
@@ -16,7 +18,9 @@ const redirectToLandingPageOnCondition = (req, res, next) => {
   logger.infoWithReq(req, `Redirect feature: ${redirectFeatureOn}`);
   logger.infoWithReq(req, 'PFE redirect check', `Case Ref: ${caseId}. Case State: ${caseState}.`);
   if (redirectFeatureOn && redirect) {
-    return res.redirect('/cutoff-landing-page');
+    const appLandingPage = `${CONF.apps.dn.url}${CONF.apps.dn.landing}`;
+    const queryString = `?${authTokenString}=${req.cookies[authTokenString]}`;
+    return res.redirect(`${appLandingPage}${queryString}`);
   }
   return next();
 };

beec8552b157fb50ccdb0c82cafbd0e50c14e2bc
Ruban <ruban72@hotmail.com>
2022-04-01 14:56:10 +0100

    DFR-981: Add redirect to screening question

--- app/steps/screening-questions/language-preference/index.js
+++ app/steps/screening-questions/language-preference/index.js
@@ -1,9 +1,17 @@
 const ScreeningValidationStep = require('app/core/steps/ScreeningValidationStep');
+const parseBool = require('../../../core/utils/parseBool');
+const CONF = require('config');
+
+const redirectFeatureOn = parseBool(CONF.features.newAppCutoff);
 
 module.exports = class ScreeningQuestionsLanguagePreference extends ScreeningValidationStep {
   get url() {
+    if (redirectFeatureOn) {
+      return '/cutoff-landing-page';
+    }
     return '/screening-questions/language-preference';
   }
+
   get nextStep() {
     return {
       languagePreferenceWelsh: {

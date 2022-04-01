8b07af67f961ce533c348990a0986ef719acae95
Ruban <ruban72@hotmail.com>
2022-04-01 15:31:01 +0100

    DFR-981: Remove redirect

--- app/steps/screening-questions/language-preference/index.js
+++ app/steps/screening-questions/language-preference/index.js
@@ -1,14 +1,7 @@
 const ScreeningValidationStep = require('app/core/steps/ScreeningValidationStep');
-const parseBool = require('../../../core/utils/parseBool');
-const CONF = require('config');
-
-const redirectFeatureOn = parseBool(CONF.features.newAppCutoff);
 
 module.exports = class ScreeningQuestionsLanguagePreference extends ScreeningValidationStep {
   get url() {
-    if (redirectFeatureOn) {
-      return '/cutoff-landing-page';
-    }
     return '/screening-questions/language-preference';
   }
 

6723f17dc5a9a9902443ddef536b57b4782c2603
Ruban <ruban72@hotmail.com>
2022-04-01 01:29:45 +0100

    DFR-981: refactpr middleware for redirect to landing page

--- app/middleware/redirectLandingPage.js
+++ app/middleware/redirectLandingPage.js
@@ -1,15 +1,21 @@
 const CONF = require('config');
-const _ = require('lodash');
 const logger = require('app/services/logger').logger(__filename);
 const parseBool = require('app/core/utils/parseBool');
 
 const redirectToLandingPageOnCondition = (req, res, next) => {
-  const session = req.session;
-  const caseId = _.get(session, 'caseId');
   const redirectFeatureOn = parseBool(CONF.features.newAppCutoff);
-  logger.infoWithReq(req, `Redirect feature: ${redirectFeatureOn}`);
+  const redirectionStates = CONF.newAppCutoffRedirectStates;
+  const { session } = req;
+  const caseState = session.state;
+  const redirect = redirectionStates.includes(caseState) || !caseState;
+  const caseId = session.caseId;
+
   logger.infoWithReq(req, `Case Ref: ${caseId}`);
-  if (redirectFeatureOn) {
+  logger.infoWithReq(req, `Redirection States: ${redirectionStates}`);
+  logger.infoWithReq(req, `Redirect: ${redirect}`);
+  logger.infoWithReq(req, `Redirect feature: ${redirectFeatureOn}`);
+  logger.infoWithReq(req, 'PFE redirect check', `Case Ref: ${caseId}. Case State: ${caseState}.`);
+  if (redirectFeatureOn && redirect) {
     return res.redirect('/cutoff-landing-page');
   }
   return next();

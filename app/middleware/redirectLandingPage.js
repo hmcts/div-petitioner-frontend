9aa12b83afb995256f31d814bb281dd6ea8636d2
Ruban <ruban72@hotmail.com>
2022-04-01 10:40:59 +0100

    DFR-981: Test redirect 6

--- app/middleware/redirectLandingPage.js
+++ app/middleware/redirectLandingPage.js
@@ -8,7 +8,7 @@ const redirectToLandingPageOnCondition = (req, res, next) => {
   logger.infoWithReq(req, 'PFE redirect check', 'Redirect straightaway');
   const { session } = req;
   logger.infoWithReq(req, `Case ref: ${session.caseId}`);
-  if (redirectFeatureOn) {
+  if (redirectFeatureOn && session.caseId) {
     return res.redirect('/cutoff-landing-page');
   }
   return next();

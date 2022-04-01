6d02dadd5bb606b93895f86490381ab579776b31
Ruban <ruban72@hotmail.com>
2022-04-01 08:47:26 +0100

    DFR-981: Test redirect 3

--- app/middleware/redirectLandingPage.js
+++ app/middleware/redirectLandingPage.js
@@ -1,14 +1,18 @@
+const CONF = require('config');
 const logger = require('app/services/logger').logger(__filename);
+const parseBool = require('app/core/utils/parseBool');
+
 
 const redirectToLandingPageOnCondition = (req, res, next) => {
+  const redirectFeatureOn = parseBool(CONF.features.newAppCutoff);
+  logger.infoWithReq(req, `Redirect feature: ${redirectFeatureOn}`);
   logger.infoWithReq(req, 'PFE redirect check', 'Redirect straightaway');
   const { session } = req;
-  // when a new case has just been submitted for the session
-  if (session.caseId) {
+  logger.infoWithReq(req, `Case ref: ${session.caseId}`);
+  if (redirectFeatureOn && session.caseId) {
     return res.redirect('/cutoff-landing-page');
   }
   return next();
-
 };
 
 module.exports = { redirectToLandingPageOnCondition };

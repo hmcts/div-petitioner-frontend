48c3264e5564e35f0d9685125ed6715f9c4f5ec0
Barry Weeks <barry.weeks@justice.gov.uk>
2022-04-01 15:04:28 +0100

    Revert "Update ScreeningValidationStep.js"
    This reverts commit cafa94cf7cd47400bc28f966b8ca9594ac279449.

--- app/core/steps/ScreeningValidationStep.js
+++ app/core/steps/ScreeningValidationStep.js
@@ -7,10 +7,6 @@ const { hasSubmitted } = require('app/middleware/submissionMiddleware');
 const { restoreFromDraftStore } = require('app/middleware/draftPetitionStoreMiddleware');
 const { idamProtect } = require('app/middleware/idamProtectMiddleware');
 const { setIdamUserDetails } = require('app/middleware/setIdamDetailsToSessionMiddleware');
-const _ = require('lodash');
-const CONF = require('config');
-
-const logger = require('app/services/logger').logger(__filename);
 
 module.exports = class ScreeningValidationStep extends ValidationStep {
   get middleware() {
@@ -26,27 +22,6 @@ module.exports = class ScreeningValidationStep extends ValidationStep {
   }
 
   get postMiddleware() {
-    const redirectLandingPage = (req, res, next) => {
-      logger.infoWithReq(req, 'Redirecting from Screening Validation');
-      const session = req.session;
-      const caseState = _.get(session, 'state');
-      const caseId = _.get(session, 'caseId');
-      logger.infoWithReq(req, caseId);
-      const redirectionStates = CONF.newAppCutoffRedirectStates;
-      logger.infoWithReq(req, `Screening Validation Redirection States: ${redirectionStates}`);
-      const redirect = redirectionStates.includes(caseState) || !caseState;
-      logger.infoWithReq(req, `Screening Validation Redirect: ${redirect}`);
-      const toggle = JSON.parse(CONF.features.newAppCutoff);
-      logger.infoWithReq(req, `Screening Validation Toggle: ${toggle}`);
-      // eslint-disable-next-line no-undefined
-      logger.infoWithReq(req, `Screening Validation TypeOf CaseId: ${typeof caseId === undefined}`);
-      if (toggle && redirect) {
-        logger.infoWithReq(req, 'PFE redirecting to Landing Page', `Case Ref: ${caseId}. Redirect check Passed.`);
-        res.redirect('/cutoff-landing-page');
-      }
-
-      return next();
-    };
-    return [redirectLandingPage];
+    return [];
   }
 };

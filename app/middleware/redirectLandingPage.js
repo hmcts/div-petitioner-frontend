a2a60f7add7777e723df4c22fcf219253ffdfb09
Ruban <ruban72@hotmail.com>
2022-04-01 08:23:14 +0100

    DFR-981: Test redirect 2

--- app/middleware/redirectLandingPage.js
+++ app/middleware/redirectLandingPage.js
@@ -1,25 +1,14 @@
-const CONF = require('config');
 const logger = require('app/services/logger').logger(__filename);
-const parseBool = require('app/core/utils/parseBool');
-
 
 const redirectToLandingPageOnCondition = (req, res, next) => {
-  const redirectFeatureOn = parseBool(CONF.features.newAppCutoff);
-  const redirectionStates = CONF.newAppCutoffRedirectStates;
+  logger.infoWithReq(req, 'PFE redirect check', 'Redirect straightaway');
   const { session } = req;
-  const caseState = session.state;
-  const redirect = redirectionStates.includes(caseState) || !caseState;
-  const caseId = session.caseId;
-
-  logger.infoWithReq(req, `Case Ref: ${caseId}`);
-  logger.infoWithReq(req, `Redirection States: ${redirectionStates}`);
-  logger.infoWithReq(req, `Redirect: ${redirect}`);
-  logger.infoWithReq(req, `Redirect feature: ${redirectFeatureOn}`);
-  logger.infoWithReq(req, 'PFE redirect check', `Case Ref: ${caseId}. Case State: ${caseState}.`);
-  if (redirectFeatureOn && redirect) {
-    return res.redirect('/contact-divorce-team');
+  // when a new case has just been submitted for the session
+  if (session.caseId) {
+    return res.redirect('/cutoff-landing-page');
   }
   return next();
+
 };
 
 module.exports = { redirectToLandingPageOnCondition };

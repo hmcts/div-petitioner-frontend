f252634da9a08fb4a124dd1e9fe2deafbdb99a27
Barry Weeks <barry.weeks@justice.gov.uk>
2022-04-04 15:35:27 +0100

    Update index.js

--- app/steps/index/index.js
+++ app/steps/index/index.js
@@ -20,7 +20,7 @@ module.exports = class Index extends Step {
   }
 
   next(ctx, session) {
-    return this.nextStep(session);
+    return this.nextStep(redirectFeatureOn, session);
   }
 
   get middleware() {

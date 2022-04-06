c0291766294706287b6a86564fb31090c52018c3
Barry Weeks <barry.weeks@justice.gov.uk>
2022-04-06 11:37:20 +0100

    Add New Step
    Add AppCutoffScreeningValidationStep

---
+++ app/core/steps/AppCutoffScreeningValidationStep.test.js
@@ -0,0 +1,40 @@
+const requireDir = require('require-directory');
+const { expect } = require('test/util/chai');
+const initSession = require('app/middleware/initSession');
+const sessionTimeout = require('app/middleware/sessionTimeout');
+const { redirectOnCondition } = require('app/middleware/redirectMiddleware');
+const { restoreFromDraftStore } = require('app/middleware/draftPetitionStoreMiddleware');
+const { idamProtect } = require('app/middleware/idamProtectMiddleware');
+const { setIdamUserDetails } = require('app/middleware/setIdamDetailsToSessionMiddleware');
+
+const modulePath = 'app/core/steps/ScreeningValidationStep';
+const UnderTest = require(modulePath);
+
+const fixtures = requireDir(module, `${__dirname}/../fixtures`);
+
+let underTest = {};
+
+describe(modulePath, () => {
+  describe('#middleware', () => {
+    it('returns middleware for screening validation step', () => {
+      underTest = new UnderTest({}, 'screening-questions', null, fixtures.content.simple, fixtures.schemas.simple);
+      const middleware = [
+        idamProtect,
+        initSession,
+        sessionTimeout,
+        redirectOnCondition,
+        restoreFromDraftStore,
+        setIdamUserDetails
+      ];
+      expect(underTest.middleware).to.eql(middleware);
+    });
+  });
+
+  describe('#postMiddleware', () => {
+    it('returns postMiddleware for validation step', () => {
+      underTest = new UnderTest({}, 'screening-questions', null, fixtures.content.simple, fixtures.schemas.simple);
+      const middleware = [];
+      expect(underTest.postMiddleware).to.eql(middleware);
+    });
+  });
+});

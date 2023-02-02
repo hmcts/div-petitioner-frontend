d683728c6ca03b93b5a15e578507beb25e301884
damongreen123 <damon.green@btinternet.com>
2023-02-02 11:19:52 +0000

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.product}-${var.component}-${var.env}"
+  name     = "${var.product}-${var.env}"
   location = var.location
 
   tags = var.common_tags

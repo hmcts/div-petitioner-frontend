83c8ec1d9a8bef833ee917c7ba400cf36bf2d5e3
Dawud Rahman <109285380+dawudgovuk@users.noreply.github.com>
2023-05-25 12:08:14 +0100

    DFR-1777 FR - Upgrade to Yarn v3 (#1084)
    * DFR-1777 FR - Upgrade to Yarn v3
    
    * update browserlist
    
    * DFR-1777 update yarn audit
    
    * DFR-1777 FR - Upgrade to Yarn v3
    
    * DFR-1777 FR - Upgrade to Yarn v3
    
    * DFR-1777 FR - Upgrade to Yarn v3
    
    * DFR-1777 FR - Upgrade to Yarn v3
    
    * DFR-1777 FR - Upgrade to Yarn v3
    
    * Merge Yarn berry test branch
    
    * Merge Yarn berry test branch
    
    * Merge Yarn berry test branch
    
    * Merge Yarn berry test branch
    
    * Merge Yarn berry test branch
    
    * Merge Yarn berry test branch
    
    * Merge Yarn berry test branch
    
    * Revert "Merge Yarn berry test branch"
    
    This reverts commit 682a4be3913210d06b8ba4c719cd7df8d98a2f93.
    
    * Merge Yarn berry test branch
    
    * Merge Yarn berry test branch
    
    * Merge Yarn berry test branch
    
    * Merge Yarn berry test branch
    
    * Merge Yarn berry test branch
    
    * Merge Yarn berry test branch
    
    * Update references to internal urls in configuration for all DIV services
    
    * Update Terraform config
    
    * Update Terraform config
    
    * Update Terraform config
    
    * Update Terraform config
    
    * Bumping chart version/ fixing aliases
    
    * Update Terraform config
    
    * Fix Unit Tests
    
    * Fix Unit Tests
    
    * Fix Unit Tests
    
    * Fix Unit Tests
    
    * Turn off automated test as causing timeout in Jenkins build to fail
    
    * Turn off automated test as causing timeout in Jenkins build to fail
    
    * Turn off automated test as causing timeout in Jenkins build to fail
    
    ---------
    
    Co-authored-by: Michael1142 <michael.johnson9@justice.gov.uk>
    Co-authored-by: hmcts-jenkins-d-to-i <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,74 +8,74 @@ data "azurerm_key_vault" "div_key_vault" {
 }
 
 data "azurerm_key_vault_secret" "frontend_secret" {
-  name      = "frontend-secret"
+  name         = "frontend-secret"
   key_vault_id = data.azurerm_key_vault.div_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "idam_secret" {
-  name      = "idam-secret"
+  name         = "idam-secret"
   key_vault_id = data.azurerm_key_vault.div_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "post_code_token" {
-  name      = "os-places-token"
+  name         = "os-places-token"
   key_vault_id = data.azurerm_key_vault.div_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "session_secret" {
-  name      = "session-secret"
+  name         = "session-secret"
   key_vault_id = data.azurerm_key_vault.div_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "redis_secret" {
-  name      = "redis-secret"
+  name         = "redis-secret"
   key_vault_id = data.azurerm_key_vault.div_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "redis_connection_string" {
-  name = "${var.component}-redis-connection-string"
-  value = "redis://ignore:${urlencode(module.redis-cache.access_key)}@${module.redis-cache.host_name}:${module.redis-cache.redis_port}?tls=true"
+  name         = "${var.component}-redis-connection-string"
+  value        = "redis://ignore:${urlencode(module.redis-cache.access_key)}@${module.redis-cache.host_name}:${module.redis-cache.redis_port}?tls=true"
   key_vault_id = data.azurerm_key_vault.div_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "launchdarkly_key" {
-  name      = "launchdarkly-key"
+  name         = "launchdarkly-key"
   key_vault_id = data.azurerm_key_vault.div_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "idamCitizenEmail" {
-  name      = "idam-citizen-username"
+  name         = "idam-citizen-username"
   key_vault_id = data.azurerm_key_vault.div_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "idamCitizenPassword" {
-  name      = "idam-citizen-password"
+  name         = "idam-citizen-password"
   key_vault_id = data.azurerm_key_vault.div_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "appinsights_secret" {
-  name = "AppInsightsInstrumentationKey"
+  name         = "AppInsightsInstrumentationKey"
   key_vault_id = data.azurerm_key_vault.div_key_vault.id
 }
 
 locals {
-  aseName                             = "core-compute-${var.env}"
-  public_hostname                     = "div-pfe-${var.env}.service.${local.aseName}.internal"
+  aseName         = "core-compute-${var.env}"
+  public_hostname = "div-pfe-${var.env}.service.${local.aseName}.internal"
 
-  local_env                           = (var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env
+  local_env = (var.env == "preview" || var.env == "spreview") ? (var.env == "preview") ? "aat" : "saat" : var.env
 
-  previewVaultName                    = "${var.reform_team}-aat"
-  nonPreviewVaultName                 = "${var.reform_team}-${var.env}"
-  vaultName                           = (var.env == "preview" || var.env == "spreview") ? local.previewVaultName : local.nonPreviewVaultName
+  previewVaultName    = "${var.reform_team}-aat"
+  nonPreviewVaultName = "${var.reform_team}-${var.env}"
+  vaultName           = (var.env == "preview" || var.env == "spreview") ? local.previewVaultName : local.nonPreviewVaultName
 
-  service_auth_provider_url           = var.service_auth_provider_url == "" ? "http://${var.idam_s2s_url_prefix}-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.service_auth_provider_url
+  service_auth_provider_url = var.service_auth_provider_url == "" ? "http://${var.idam_s2s_url_prefix}-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.service_auth_provider_url
 
-  case_orchestration_service_url      = var.case_orchestration_service_url == "" ? "http://div-cos-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.case_orchestration_service_url
-  evidence_management_client_api_url  = var.evidence_management_client_api_url == "" ? "http://div-emca-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.evidence_management_client_api_url
-  fees_and_payments_url               = var.fees_and_payments_url == "" ? "http://div-fps-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.fees_and_payments_url
-  decree_nisi_frontend_url            = var.decree_nisi_frontend_url == "" ? "https://div-dn-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.decree_nisi_frontend_url
+  case_orchestration_service_url     = var.case_orchestration_service_url == "" ? "http://div-cos-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.case_orchestration_service_url
+  evidence_management_client_api_url = var.evidence_management_client_api_url == "" ? "http://div-emca-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.evidence_management_client_api_url
+  fees_and_payments_url              = var.fees_and_payments_url == "" ? "http://div-fps-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.fees_and_payments_url
+  decree_nisi_frontend_url           = var.decree_nisi_frontend_url == "" ? "https://div-dn-${local.local_env}.service.core-compute-${local.local_env}.internal" : var.decree_nisi_frontend_url
 
-  status_health_endpoint             = "/status/health"
+  status_health_endpoint = "/status/health"
 
   asp_name = var.env == "prod" ? "div-pfe-prod" : "${var.raw_product}-${var.env}"
   asp_rg   = var.env == "prod" ? "div-pfe-prod" : "${var.raw_product}-${var.env}"

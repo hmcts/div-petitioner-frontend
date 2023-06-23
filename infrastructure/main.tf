76a12a2be7a9168144f31a674a2ea01339773e6e
Mike Lemos <gobii@me.com>
2023-06-23 12:56:39 +0100

    Dfr 1199 upgrade to redis6 (#1114)
    * DFR-1199 upgrade to redis 6
    
    * DFR-1199 removing the nvmrc warning
    
    * DFR-1199 upgrading node
    
    * DFR-1199 reverting node upgrade
    
    * DFR-1199 reverting node upgrade
    
    * DFR-1999 suppressing cves
    
    * DFR-1999 suppressing cves
    
    * DFR-1999 suppressing cves
    
    * DFR-1999 suppressing cves
    
    * DFR-1999 fixing concurrent instances
    
    * DFR-1999 fixing concurrent instances
    
    * DFR-1999 fixing redis references
    
    * Bumping chart version/ fixing aliases
    
    * DFR-1999 fixing redis references
    
    * DFR-1999 removing redis declaration
    
    * DFR-1999 security checks fixed
    
    ---------
    
    Co-authored-by: hmcts-jenkins-d-to-i <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -32,12 +32,6 @@ data "azurerm_key_vault_secret" "redis_secret" {
   key_vault_id = data.azurerm_key_vault.div_key_vault.id
 }
 
-resource "azurerm_key_vault_secret" "redis_connection_string" {
-  name         = "${var.component}-redis-connection-string"
-  value        = "redis://ignore:${urlencode(module.redis-cache.access_key)}@${module.redis-cache.host_name}:${module.redis-cache.redis_port}?tls=true"
-  key_vault_id = data.azurerm_key_vault.div_key_vault.id
-}
-
 data "azurerm_key_vault_secret" "launchdarkly_key" {
   name         = "launchdarkly-key"
   key_vault_id = data.azurerm_key_vault.div_key_vault.id
@@ -88,19 +82,6 @@ data "azurerm_subnet" "core_infra_redis_subnet" {
   name                 = "core-infra-subnet-1-${var.env}"
   virtual_network_name = "core-infra-vnet-${var.env}"
   resource_group_name  = "core-infra-${var.env}"
-}
-
-module "redis-cache" {
-  source   = "git@github.com:hmcts/cnp-module-redis?ref=master"
-  product  = var.product
-  name     = "${var.product}-${var.component}-${var.env}"
-  location = var.location
-  env      = var.env
-  private_endpoint_enabled = true
-  redis_version = "6"
-  business_area = "cft"
-  public_network_access_enabled = false
-  common_tags  = var.common_tags
 }
 
 

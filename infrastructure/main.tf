102e196526fff497bd6b108cf6c674d65710f8e1
Mike Lemos <gobii@me.com>
2023-06-23 11:47:36 +0100

    DFR-1999 removing redis declaration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -84,16 +84,4 @@ data "azurerm_subnet" "core_infra_redis_subnet" {
   resource_group_name  = "core-infra-${var.env}"
 }
 
-module "redis-cache" {
-  source      = "git@github.com:hmcts/cnp-module-redis?ref=master"
-  product     = var.env != "preview" ? "${var.product}-redis" : "${var.product}-${var.component}-redis"
-  location    = var.location
-  env         = var.env
-  private_endpoint_enabled = true
-  redis_version = "6"
-  business_area = "cft"
-  public_network_access_enabled = false
-  common_tags = var.common_tags
-}
-
 
